78483672a866c712c6bab933cd63d862e1f3c052
Ehsan Totoni <ehsan.totoni@intel.com>
2017-06-06 07:29:21 -0700

    linear regression example, minor

--- examples/kernel_density_estimation.py
+++ examples/kernel_density_estimation.py
@@ -7,8 +7,8 @@ import time
 
 @hpat.jit(locals={'X':hpat.float64[:]})
 def kde():
-    f = h5py.File("lr.hdf5", "r")
-    X = f['responses'][:]
+    f = h5py.File("kde.hdf5", "r")
+    X = f['points'][:]
     b = 0.5
     points = np.array([-1.0, 2.0, 5.0])
     N = points.shape[0]
@@ -26,9 +26,9 @@ def kde():
 
 def main():
     parser = argparse.ArgumentParser(description='Kernel-Density')
-    parser.add_argument('--size', dest='size', type=int, default=1000000)
+    parser.add_argument('--file', dest='file', type=str, default="lr.hdf5")
     args = parser.parse_args()
-    size = args.size
+    file_name = args.file
 
     res = kde()
 
---
+++ examples/linear_regression.py
@@ -0,0 +1,38 @@
+import hpat
+import numpy as np
+import h5py
+import argparse
+import time
+
+@hpat.jit(locals={'X':hpat.float64[:,:], 'Y':hpat.float64[:,:]})
+def linear_regression(iterations):
+    f = h5py.File("lir.hdf5", "r")
+    X = f['points'][:]
+    Y = f['responses'][:]
+    N,D = X.shape
+    p = Y.shape[1]
+    alphaN = 0.01/N
+    w = np.zeros((D,p))
+    t1 = time.time()
+    for i in range(iterations):
+        w -= alphaN * np.dot(X.T, np.dot(X,w)-Y)
+    t2 = time.time()
+    print("Execution time:", t2-t1,"\nresult:")
+    for i in range(D):
+        for j in range(p):
+            print(w[i,j]," ")
+    return w
+
+def main():
+    parser = argparse.ArgumentParser(description='Linear Regression.')
+    parser.add_argument('--file', dest='file', type=str, default="lr.hdf5")
+    parser.add_argument('--iterations', dest='iterations', type=int, default=30)
+    args = parser.parse_args()
+
+    file_name = args.file
+    iterations = args.iterations
+
+    w = linear_regression(iterations)
+
+if __name__ == '__main__':
+    main()
