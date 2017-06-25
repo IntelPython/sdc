64f8086706f972506aa450244b776d79d14a5b7a
Ehsan Totoni <ehsan.totoni@intel.com>
2017-06-25 10:40:25 -0700

    minor example

--- examples/hiframes_filter.py
+++ examples/hiframes_filter.py
@@ -5,7 +5,7 @@ import hpat
 @hpat.jit
 def filter_df(n):
     df = pd.DataFrame({'A': np.random.ranf(10),'B': np.random.ranf(10)})
-    df1 = df[df.A>.5]
+    df1 = df[df.A > .5]
     return np.sum(df1.B)
 
 n = 10
