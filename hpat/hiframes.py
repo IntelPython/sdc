c3e6af78f6f24f3a2777df82fc078dd2f95cf61b
Ehsan Totoni <ehsan.totoni@intel.com>
2017-07-11 13:09:47 -0700

    refactor hiframes api

--- hpat/hiframes.py
+++ hpat/hiframes.py
@@ -1,106 +1,13 @@
 from __future__ import print_function, division, absolute_import
-import types as pytypes # avoid confusion with numba.types
+import types as pytypes  # avoid confusion with numba.types
 
 import numba
-from numba import ir, analysis, types, config, numpy_support, typeinfer
+from numba import ir, config
 from numba.ir_utils import (mk_unique_var, replace_vars_inner, find_topo_order,
                             dprint_func_ir, remove_dead, mk_alloc)
-from numba.typing import signature
-from hpat import distributed, distributed_analysis
-from hpat.distributed_analysis import Distribution
+import hpat
+from hpat import hiframes_api
 import pandas
-
-class Filter(ir.Stmt):
-    def __init__(self, df_out, df_in, bool_arr, df_vars, loc):
-        self.df_out = df_out
-        self.df_in = df_in
-        self.bool_arr = bool_arr
-        # needs df columns for type inference stage
-        self.df_vars = df_vars
-        self.loc = loc
-
-    def __repr__(self):
-        return "filter: {} = {}[{}]".format(self.df_out, self.df_in,
-                                                                self.bool_arr)
-
-def filter_array_analysis(filter_node, array_analysis):
-    df_vars = filter_node.df_vars
-    df_in_vars = df_vars[filter_node.df_in]
-    df_out_vars = df_vars[filter_node.df_out]
-
-    # arrays of input df have same size in last dimension
-    c_in = array_analysis._get_next_class()
-    for _, col_var in df_in_vars.items():
-        c_in = array_analysis._merge_classes(c_in,
-                            array_analysis.array_shape_classes[col_var.name][0])
-
-    # create correlations for output arrays
-    for _, col_var in df_out_vars.items():
-        array_analysis._add_array_corr(col_var.name)
-
-    # arrays of output df have same size in last dimension
-    c_out = array_analysis._get_next_class()
-    for _, col_var in df_out_vars.items():
-        c_out = array_analysis._merge_classes(c_out,
-                            array_analysis.array_shape_classes[col_var.name][0])
-
-    # gen size variable for an output column
-    out_col = list(df_out_vars.items())[0][1]
-    size_nodes = array_analysis._gen_size_call(out_col, 0)
-    size_var = size_nodes[-1].target
-    array_analysis.class_sizes[c_out] = [size_var]
-    return size_nodes
-
-numba.array_analysis.array_analysis_extensions[Filter] = filter_array_analysis
-
-def filter_distributed_analysis(filter_node, array_dists):
-    df_vars = filter_node.df_vars
-    df_in_vars = df_vars[filter_node.df_in]
-    df_out_vars = df_vars[filter_node.df_out]
-
-    # input columns have same distribution
-    in_dist = Distribution.OneD
-    for _, col_var in df_in_vars.items():
-        in_dist = Distribution(min(in_dist.value, array_dists[col_var.name].value))
-    for _, col_var in df_in_vars.items():
-        array_dists[col_var.name] = in_dist
-
-    # output columns have same distribution
-    out_dist = Distribution.OneD_Var
-    for _, col_var in df_out_vars.items():
-        # output dist might not be assigned yet
-        if col_var.name in array_dists:
-            out_dist = Distribution(min(out_dist.value, array_dists[col_var.name].value))
-    for _, col_var in df_out_vars.items():
-        array_dists[col_var.name] = out_dist
-
-    return
-
-distributed_analysis.distributed_analysis_extensions[Filter] = filter_distributed_analysis
-
-def filter_distributed_run(filter_node, typemap, calltypes):
-    # TODO: rebalance if output distributions are 1D instead of 1D_Var
-    df_vars = filter_node.df_vars
-    df_in_vars = df_vars[filter_node.df_in]
-    df_out_vars = df_vars[filter_node.df_out]
-    loc = filter_node.loc
-    bool_arr = filter_node.bool_arr
-
-    out = []
-    for col_name, col_in_var in df_in_vars.items():
-        col_out_var = df_out_vars[col_name]
-        # using getitem like Numba for filtering arrays
-        # TODO: generate parfor
-        getitem_call = ir.Expr.getitem(col_in_var, bool_arr, loc)
-        calltypes[getitem_call] = signature(
-                typemap[col_out_var.name],  # output type
-                typemap[col_in_var.name],  # input type
-                typemap[bool_arr.name])  # index type
-        out.append(ir.Assign(getitem_call, col_out_var, loc))
-
-    return out
-
-distributed.distributed_run_extensions[Filter] = filter_distributed_run
 
 class HiFrames(object):
     """analyze and transform hiframes calls"""
@@ -144,7 +51,8 @@ class HiFrames(object):
 
         if isinstance(rhs, ir.Expr):
             # df_call = pd.DataFrame
-            if rhs.op=='getattr' and rhs.value.name in self.pd_globals and rhs.attr=='DataFrame':
+            if (rhs.op=='getattr' and rhs.value.name in self.pd_globals
+                    and rhs.attr=='DataFrame'):
                 self.pd_df_calls.append(lhs)
 
             # df = pd.DataFrame(map_var)
@@ -172,7 +80,8 @@ class HiFrames(object):
                 for col, _ in self.df_vars[rhs.value.name].items():
                     self.df_vars[lhs][col] = ir.Var(scope, mk_unique_var(col),
                                                                             loc)
-                return [Filter(lhs, rhs.value.name, rhs.index, self.df_vars, rhs.loc)]
+                return [hiframes_api.Filter(lhs, rhs.value.name, rhs.index,
+                                                        self.df_vars, rhs.loc)]
 
             # d = df.column
             if rhs.op=='getattr' and rhs.value.name in self.df_vars:
@@ -199,16 +108,3 @@ class HiFrames(object):
             col_name = self.str_const_table[col_var]
             df_cols[col_name] = item[1]
         return df_cols
-
-
-def filter_typeinfer(filter_node, typeinferer):
-    df_vars = filter_node.df_vars
-    df_in_vars = df_vars[filter_node.df_in]
-    df_out_vars = df_vars[filter_node.df_out]
-    for col_name, col_var in df_in_vars.items():
-        out_col_var = df_out_vars[col_name]
-        typeinferer.constraints.append(typeinfer.Propagate(dst=out_col_var.name,
-                                              src=col_var.name, loc=filter_node.loc))
-    return
-
-typeinfer.typeinfer_extensions[Filter] = filter_typeinfer
