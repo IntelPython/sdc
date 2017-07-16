0ddfbd68a0f2ca7baa333a9245134bd59949e000
Ehsan Totoni <ehsan.totoni@intel.com>
2017-07-16 15:00:27 -0700

    update rolling example

--- examples/hiframes_rolling.py
+++ examples/hiframes_rolling.py
@@ -5,14 +5,20 @@ import hpat
 @hpat.jit
 def rolling_df1(n):
     df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
-    Ac = df.A.rolling(3).sum()
+    Ac = df.A.rolling(5).sum()
     return Ac.sum()
 
 @hpat.jit
 def rolling_df2(n):
     df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
-    Ac = df.A.rolling(3, center=True).sum()
+    Ac = df.A.rolling(5, center=True).mean()
+    return Ac.sum()
+
+@hpat.jit
+def rolling_df3(n):
+    df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
+    Ac = df.A.rolling(3, center=True).apply(lambda a: a[0]+2*a[1]+a[2])
     return Ac.sum()
 
 n = 10
-print(rolling_df2(n))
+print(rolling_df3(n))
