36551e1d2a2ef83ddd756ad068d9558e7e7b5d53
Ehsan Totoni <ehsan.totoni@intel.com>
2017-07-15 19:50:14 -0700

    rolling center example

--- examples/hiframes_rolling.py
+++ examples/hiframes_rolling.py
@@ -3,10 +3,16 @@ import numpy as np
 import hpat
 
 @hpat.jit
-def cumsum_df(n):
+def rolling_df1(n):
     df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
     Ac = df.A.rolling(3).sum()
     return Ac.sum()
 
+@hpat.jit
+def rolling_df2(n):
+    df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
+    Ac = df.A.rolling(3, center=True).sum()
+    return Ac.sum()
+
 n = 10
-print(cumsum_df(n))
+print(rolling_df2(n))
