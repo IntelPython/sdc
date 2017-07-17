aede039d3df70f4f50b2d92dc429f8414815cff1
Ehsan Totoni <ehsan.totoni@intel.com>
2017-07-17 11:42:25 -0700

    pct_change example

--- examples/hiframes_shift.py
+++ examples/hiframes_shift.py
@@ -3,10 +3,16 @@ import numpy as np
 import hpat
 
 @hpat.jit
-def shift_df(n):
+def shift_df1(n):
     df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
     Ac = df.A.shift(1)
     return Ac.sum()
 
+@hpat.jit
+def shift_df2(n):
+    df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
+    Ac = df.A.pct_change()
+    return Ac.sum()
+
 n = 10
-print(shift_df(n))
+print(shift_df2(n))
