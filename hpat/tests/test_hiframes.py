aaa9730db705e6697ff1165b198ad2d210e62e00
Ehsan Totoni <ehsan.totoni@intel.com>
2017-08-28 10:45:09 -0700

    utils to count array parfor 1d/rep

--- hpat/tests/test_hiframes.py
+++ hpat/tests/test_hiframes.py
@@ -2,6 +2,8 @@ import unittest
 import pandas as pd
 import numpy as np
 import hpat
+from hpat.tests.test_utils import (count_array_REPs, count_parfor_REPs,
+                            count_parfor_OneDs, count_array_OneDs)
 
 class TestHiFrames(unittest.TestCase):
     def test_cumsum(self):
@@ -13,6 +15,10 @@ class TestHiFrames(unittest.TestCase):
         hpat_func = hpat.jit(test_impl)
         n = 11
         self.assertEqual(hpat_func(n), test_impl(n))
+        self.assertEqual(count_array_REPs(), 0)
+        self.assertEqual(count_array_OneDs(), 2)
+        self.assertEqual(count_parfor_REPs(), 0)
+        self.assertEqual(count_parfor_OneDs(), 2)
 
 if __name__ == "__main__":
     unittest.main()
---
+++ hpat/tests/test_utils.py
@@ -0,0 +1,21 @@
+import hpat
+
+def count_array_REPs():
+    from hpat.distributed import Distribution
+    vals = hpat.distributed.dist_analysis.array_dists.values()
+    return sum([v==Distribution.REP for v in vals])
+
+def count_parfor_REPs():
+    from hpat.distributed import Distribution
+    vals = hpat.distributed.dist_analysis.parfor_dists.values()
+    return sum([v==Distribution.REP for v in vals])
+
+def count_parfor_OneDs():
+    from hpat.distributed import Distribution
+    vals = hpat.distributed.dist_analysis.parfor_dists.values()
+    return sum([v==Distribution.OneD for v in vals])
+
+def count_array_OneDs():
+    from hpat.distributed import Distribution
+    vals = hpat.distributed.dist_analysis.array_dists.values()
+    return sum([v==Distribution.OneD for v in vals])
