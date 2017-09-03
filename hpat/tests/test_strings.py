ecb1cf69402efed881edbf5f3a69178c13073cd2
Ehsan Totoni <ehsan.totoni@intel.com>
2017-09-02 19:59:50 -0700

    test str concat, split

--- hpat/tests/test_strings.py
+++ hpat/tests/test_strings.py
@@ -30,5 +30,19 @@ class TestString(unittest.TestCase):
         hpat_func = hpat.jit(test_impl)
         self.assertEqual(hpat_func(arg), test_impl(arg))
 
+    def test_concat(self):
+        def test_impl(_str):
+            return (_str+'test_str')
+        hpat_func = hpat.jit(test_impl)
+        arg = 'a_'
+        self.assertEqual(hpat_func(arg), test_impl(arg))
+
+    def test_split(self):
+        def test_impl(_str):
+            return _str.split('/')
+        hpat_func = hpat.jit(test_impl)
+        arg = 'aa/bb/cc'
+        self.assertEqual(hpat_func(arg), test_impl(arg))
+
 if __name__ == "__main__":
     unittest.main()
