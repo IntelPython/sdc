d5ff7f4e6c73d9b3800cb99f12f55af64dfb8fb4
Ehsan Totoni <ehsan.totoni@intel.com>
2017-09-21 07:42:02 -0700

    parquet examples

---
+++ examples/kernel_density_estimation_pq.py
@@ -0,0 +1,35 @@
+import pyarrow.parquet as pq
+import hpat
+from hpat import prange
+import numpy as np
+import argparse
+import time
+
+#@hpat.jit
+def kde():
+    t = pq.read_table('kde.parquet')
+    df = t.to_pandas()
+    X = df['points'].values
+    b = 0.5
+    points = np.array([-1.0, 2.0, 5.0])
+    N = points.shape[0]
+    n = X.shape[0]
+    exps = 0
+    t1 = time.time()
+    for i in prange(n):
+        p = X[i]
+        d = (-(p-points)**2)/(2*b**2)
+        m = np.min(d)
+        exps += m-np.log(b*N)+np.log(np.sum(np.exp(d-m)))
+    t = time.time()-t1
+    print("Execution time:", t,"\nresult:", exps)
+    return exps
+
+def main():
+    parser = argparse.ArgumentParser(description='Kernel-Density')
+    args = parser.parse_args()
+
+    res = kde()
+
+if __name__ == '__main__':
+    main()
---
+++ generate_data/gen_kde_pq.py
@@ -0,0 +1,27 @@
+import pyarrow.parquet as pq
+import pyarrow as pa
+import numpy as np
+import pandas as pd
+import argparse
+import time
+import hpat
+
+def gen_kde(N, file_name):
+    # np.random.seed(0)
+    df = pd.DataFrame({'points': np.random.random(N)})
+    table = pa.Table.from_pandas(df)
+    row_group_size = 128
+    pq.write_table(table, 'kde.parquet', row_group_size)
+
+def main():
+    parser = argparse.ArgumentParser(description='Gen KDE.')
+    parser.add_argument('--size', dest='size', type=int, default=2000)
+    parser.add_argument('--file', dest='file', type=str, default="kde.hdf5")
+    args = parser.parse_args()
+    N = args.size
+    file_name = args.file
+
+    gen_kde(N, file_name)
+
+if __name__ == '__main__':
+    main()
