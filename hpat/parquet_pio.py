d021bebe1f52c6af1e165502884c76ae65b589f4
Ehsan Totoni <ehsan.totoni@intel.com>
2017-10-05 15:17:27 -0700

    refactor read_parquet for strings

--- hpat/parquet_pio.py
+++ hpat/parquet_pio.py
@@ -25,6 +25,9 @@ _pq_type_to_numba = {'BOOLEAN': types.Array(types.boolean, 1, 'C'),
 def read_parquet():
     return 0
 
+def read_parquet_str():
+    return 0
+
 def read_parquet_parallel():
     return 0
 
@@ -36,6 +39,8 @@ def remove_parquet(rhs, lives, call_list):
     if call_list == [read_parquet] and rhs.args[2].name not in lives:
         return True
     if call_list == [get_column_size_parquet]:
+        return True
+    if call_list == [read_parquet_str]:
         return True
     return False
 
@@ -84,7 +89,7 @@ def get_column_read_nodes(c_type, cvar, file_name_str, i):
     # generate strings differently
     if c_type == string_array_type:
         # pass size for easier allocation and distributed analysis
-        func_text += '  column = read_parquet("{}", {}, col_size)\n'.format(
+        func_text += '  column = read_parquet_str("{}", {}, col_size)\n'.format(
                                                             file_name_str, i)
     else:
         el_type = get_element_type(c_type.dtype)
@@ -97,7 +102,8 @@ def get_column_read_nodes(c_type, cvar, file_name_str, i):
     size_func = loc_vars['f']
     _, f_block = compile_to_numba_ir(size_func,
                 {'get_column_size_parquet': get_column_size_parquet,
-                'read_parquet': read_parquet, 'np': np,
+                'read_parquet': read_parquet,
+                'read_parquet_str': read_parquet_str, 'np': np,
                 'StringArray': StringArray}).blocks.popitem()
 
     out_nodes = f_block.body[:-3]
@@ -149,6 +155,13 @@ class ReadParquetInfer(AbstractTemplate):
             return signature(string_array_type, *args)
         # array_ty = types.Array(ndim=1, layout='C', dtype=args[2])
         return signature(types.int32, *args)
+
+@infer_global(read_parquet_str)
+class ReadParquetInfer(AbstractTemplate):
+    def generic(self, args, kws):
+        assert not kws
+        assert len(args)==3
+        return signature(string_array_type, *args)
 
 @infer_global(read_parquet_parallel)
 class ReadParallelParquetInfer(AbstractTemplate):
@@ -204,7 +217,7 @@ def pq_read_parallel_lower(context, builder, sig, args):
             args[3], args[4]])
 
 # read strings
-@lower_builtin(read_parquet, StringType, types.intp, types.intp)
+@lower_builtin(read_parquet_str, StringType, types.intp, types.intp)
 def pq_read_string_lower(context, builder, sig, args):
     typ = sig.return_type
     string_array = cgutils.create_struct_proxy(typ)(context, builder)
