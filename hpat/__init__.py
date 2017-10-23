f20c893130432d997e7b343077d0f9d28b887c94
Ehsan Totoni <ehsan.totoni@intel.com>
2017-10-22 17:19:46 -0700

    export List

--- hpat/__init__.py
+++ hpat/__init__.py
@@ -7,6 +7,7 @@ import hpat.distributed_api
 from hpat.distributed_api import dist_time
 from hpat.dict_ext import DictIntInt, DictInt32Int32, dict_int_int_type, dict_int32_int32_type
 from hpat.str_ext import string_type
+from numba.types import List
 from hpat.utils import cprint
 
 def jit(signature_or_function=None, **options):
