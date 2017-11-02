fb8f58fde75dce83e5a71957c8b1c09c74769fb5
Ehsan Totoni <ehsan.totoni@intel.com>
2017-11-02 13:47:18 -0400

    svc daal predict

--- hpat/_daal.cpp
+++ hpat/_daal.cpp
@@ -6,7 +6,9 @@ using namespace daal;
 using namespace daal::algorithms;
 using namespace daal::data_management;
 
-void svc_train(int64_t num_features, int64_t num_samples, double* X, double *y);
+void* svc_train(int64_t num_features, int64_t num_samples, double* X, double *y);
+void svc_predict(void* model_ptr, int64_t num_features, int64_t num_samples, double* p, double *res);
+void dtor_svc(void* model_ptr, int64_t size, void* in);
 
 PyMODINIT_FUNC PyInit_daal_wrapper(void) {
     PyObject *m;
@@ -18,11 +20,16 @@ PyMODINIT_FUNC PyInit_daal_wrapper(void) {
 
     PyObject_SetAttrString(m, "svc_train",
                             PyLong_FromVoidPtr((void*)(&svc_train)));
+    PyObject_SetAttrString(m, "svc_predict",
+                            PyLong_FromVoidPtr((void*)(&svc_predict)));
+    PyObject_SetAttrString(m, "dtor_svc",
+                            PyLong_FromVoidPtr((void*)(&dtor_svc)));
 
     return m;
 }
 
-void svc_train(int64_t num_features, int64_t num_samples, double* X, double *y)
+
+void* svc_train(int64_t num_features, int64_t num_samples, double* X, double *y)
 {
     // printf("svn_train nFeatures:%ld nSamples:%ld X[0]:%lf y[0]:%lf\n", num_features, num_samples, X[0], y[0]);
     services::SharedPtr<svm::training::Batch<> > training(new svm::training::Batch<>());
@@ -33,8 +40,10 @@ void svc_train(int64_t num_features, int64_t num_samples, double* X, double *y)
     training->parameter.kernel = kernel;
     prediction->parameter.kernel = kernel;
 
-    services::SharedPtr< HomogenNumericTable< double > > trainData = HomogenNumericTable<double>::create(X, num_features, num_samples);
-    services::SharedPtr< HomogenNumericTable< double > > trainGroundTruth = HomogenNumericTable<double>::create(y, 1, num_samples);
+    services::SharedPtr< HomogenNumericTable< double > > trainData =
+            HomogenNumericTable<double>::create(X, num_features, num_samples);
+    services::SharedPtr< HomogenNumericTable< double > > trainGroundTruth =
+                        HomogenNumericTable<double>::create(y, 1, num_samples);
     // printf("label rows: %ld\n", trainGroundTruth->getNumberOfRows());
 
     multi_class_classifier::training::Batch<> algorithm;
@@ -50,5 +59,57 @@ void svc_train(int64_t num_features, int64_t num_samples, double* X, double *y)
 
 
     trainingResult = algorithm.getResult();
+    // FIXME: return pointer to SharedPtr since get/set functions don't work
+    services::SharedPtr<multi_class_classifier::training::Result> * ptres =
+        new services::SharedPtr<multi_class_classifier::training::Result>();
+    *ptres = trainingResult;
+    return ptres;
+}
+
+void svc_predict(void* model_ptr, int64_t num_features, int64_t num_samples, double* p, double *res)
+{
+
+    services::SharedPtr<multi_class_classifier::training::Result>* trainingResultPtr =
+        (services::SharedPtr<multi_class_classifier::training::Result>*)(model_ptr);
+    services::SharedPtr<classifier::prediction::Result> predictionResult;
+    services::SharedPtr<svm::training::Batch<> > training(new svm::training::Batch<>());
+    services::SharedPtr<svm::prediction::Batch<> > prediction(new svm::prediction::Batch<>());
+    services::SharedPtr<kernel_function::KernelIface> kernel(new kernel_function::linear::Batch<>());
+
+    training->parameter.cacheSize = 100000000;
+    training->parameter.kernel = kernel;
+    prediction->parameter.kernel = kernel;
+
+    services::SharedPtr< HomogenNumericTable< double > > testData =
+            HomogenNumericTable<double>::create(p, num_features, num_samples);
+
+    multi_class_classifier::prediction::Batch<> algorithm;
+
+    algorithm.parameter.nClasses = 2; // FIXME
+    algorithm.parameter.training = training;
+    algorithm.parameter.prediction = prediction;
+
+    algorithm.input.set(classifier::prediction::data, testData);
+    algorithm.input.set(classifier::prediction::model,
+                        (*trainingResultPtr)->get(classifier::training::model));
+
+    algorithm.compute();
+
+    predictionResult = algorithm.getResult();
+    NumericTablePtr res_table = predictionResult->get(classifier::prediction::prediction);
+    BlockDescriptor<double> block1;
+    res_table->getBlockOfRows(0, num_samples, readOnly, block1);
+    double *data_ptr = block1.getBlockPtr();
+    // printf("%lf %lf\n", data_ptr[0], data_ptr[1]);
+    memcpy(res, data_ptr, num_samples*sizeof(double));
+    res_table->releaseBlockOfRows(block1);
+    return;
+}
+
+void dtor_svc(void* model_ptr, int64_t size, void* in)
+{
+    services::SharedPtr<multi_class_classifier::training::Result>* trainingResultPtr =
+        (services::SharedPtr<multi_class_classifier::training::Result>*)(model_ptr);
+    //delete trainingResultPtr;
     return;
 }
