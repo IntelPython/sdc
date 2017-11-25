e410969fd9e19c8f20668e1122d756411df3ece7
Ehsan Totoni <ehsan.totoni@intel.com>
2017-11-25 09:38:25 -0800

    use arange in examples

--- examples/hiframes_cumsum.py
+++ examples/hiframes_cumsum.py
@@ -4,7 +4,7 @@ import hpat
 
 @hpat.jit
 def cumsum_df(n):
-    df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
+    df = pd.DataFrame({'A': np.arange(n)+1.0, 'B': np.random.ranf(n)})
     Ac = df.A.cumsum()
     return Ac.sum()
 
--- examples/hiframes_rolling.py
+++ examples/hiframes_rolling.py
@@ -4,21 +4,26 @@ import hpat
 
 @hpat.jit
 def rolling_df1(n):
-    df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
+    df = pd.DataFrame({'A': np.arange(n)+1.0, 'B': np.random.ranf(n)})
     Ac = df.A.rolling(5).sum()
     return Ac.sum()
 
 @hpat.jit
 def rolling_df2(n):
-    df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
+    df = pd.DataFrame({'A': np.arange(n)+1.0, 'B': np.random.ranf(n)})
     df['moving average'] = df.A.rolling(window=5, center=True).mean()
     return df['moving average'].sum()
 
 @hpat.jit
 def rolling_df3(n):
-    df = pd.DataFrame({'A': np.ones(n), 'B': np.random.ranf(n)})
+    df = pd.DataFrame({'A': np.arange(n)+1.0, 'B': np.random.ranf(n)})
     Ac = df.A.rolling(3, center=True).apply(lambda a: a[0]+2*a[1]+a[2])
     return Ac.sum()
 
 n = 10
+print("sum left window:")
+print(rolling_df1(n))
+print("mean center window:")
+print(rolling_df2(n))
+print("custom center window:")
 print(rolling_df3(n))
