8e8f0fc8bdd0f2b12afa8e4f0263ea623e8aff38
Ehsan Totoni <ehsan.totoni@intel.com>
2017-12-04 14:54:59 -0800

    add daal to conda recipe

--- buildscripts/hpat-conda-recipe/build.sh
+++ buildscripts/hpat-conda-recipe/build.sh
@@ -1,4 +1,4 @@
 # LDSHARED="mpicxx -cxx=$GXX -shared" LD="mpicxx -cxx=$GXX" \
 # CC="mpicxx -cxx=$GXX -std=c++11" GXX="mpicxx -cxx=$GXX -std=c++11" \
-HDF5_DIR="${PREFIX}" MACOSX_DEPLOYMENT_TARGET=10.9 \
+DAALROOT="${PREFIX}" HDF5_DIR="${PREFIX}" MACOSX_DEPLOYMENT_TARGET=10.9 \
 $PYTHON setup.py build install --single-version-externally-managed --record=record.txt
