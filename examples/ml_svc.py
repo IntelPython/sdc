55129dcca3cbbe16b9557bbf789d277a0de4ace6
Ehsan Totoni <ehsan.totoni@intel.com>
2017-12-04 14:54:18 -0800

    more classes in SVC example

--- examples/ml_svc.py
+++ examples/ml_svc.py
@@ -2,6 +2,7 @@ import numpy as np
 import hpat
 from hpat import prange
 import hpat.ml
+import time
 
 hpat.multithread_mode = True
 
@@ -10,14 +11,16 @@ def f(N, D, M):
     X = np.random.ranf((N, D))
     y = np.empty(N)
     for i in prange(N):
-        y[i] = i%2
+        y[i] = i%4
     p = np.random.ranf((M, D))
-    clf = hpat.ml.SVC(n_classes=2)
+    clf = hpat.ml.SVC(n_classes=4)
+    t1 = time.time()
     clf.train(X, y)
     res = clf.predict(p)
+    print("Exec time:", time.time()-t1)
     return res.sum()
 
-N = 1024
+N = 1024*16
 D = 20
 M = 128
 
