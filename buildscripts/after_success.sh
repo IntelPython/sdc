cf20ae0d7f3a63c5467f64a33e9f68a7c6f196e7
Ehsan Totoni <ehsan.totoni@intel.com>
2017-12-11 19:05:32 -0800

    verbose coveralls

--- buildscripts/after_success.sh
+++ buildscripts/after_success.sh
@@ -2,10 +2,7 @@
 
 source activate $CONDA_ENV
 
-# Make sure any error below is reported as such
-set -v -e
-
 if [ "$RUN_COVERAGE" == "yes" ]; then
     coverage combine
-    coveralls
+    coveralls -v
 fi
