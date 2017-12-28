03d4fc60f4e3f11a7bfaef313111917cbed026ea
Ehsan Totoni <ehsan.totoni@intel.com>
2017-12-28 12:34:11 -0500

    parquet reader update recipe

--- buildscripts/parquet-reader-conda-recipe/build.sh
+++ buildscripts/parquet-reader-conda-recipe/build.sh
@@ -1,3 +1,8 @@
-#./configure --prefix="${PREFIX}"
-make
+mkdir -p build
+pushd build
+rm -rf *
+cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DPREFIX=$PREFIX ..
+make VERBOSE=1
 make install
+popd
