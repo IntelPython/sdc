9528fef93762dec83255d936acd5bf34d48f4cea
Ehsan Totoni <ehsan.tn@gmail.com>
2017-12-30 15:53:35 -0800

    parquet reader windows recipe

--- buildscripts/parquet-reader-conda-recipe/bld.bat
+++ buildscripts/parquet-reader-conda-recipe/bld.bat
@@ -1,3 +1,15 @@
 
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE:STRING=RELEASE ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_LIBDIR=%LIBRARY_PREFIX%/lib -DPREFIX=%LIBRARY_PREFIX% ..
+if errorlevel 1 exit 1
+
 nmake
+if errorlevel 1 exit 1
+
+nmake install
 if errorlevel 1 exit 1
