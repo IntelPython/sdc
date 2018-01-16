254b6548fe42da4b73f7e19fece01fae4af4c709
Ehsan Totoni <ehsan.totoni@intel.com>
2018-01-16 12:49:23 -0500

    ros get_image_dims lower

--- hpat/_ros.cpp
+++ hpat/_ros.cpp
@@ -6,6 +6,7 @@
 
 void* open_bag(std::string* fname);
 int64_t get_msg_count(rosbag::Bag* bag);
+void get_image_dims(int64_t* out_dims, rosbag::Bag* bag);
 
 PyMODINIT_FUNC PyInit_ros_cpp(void) {
     PyObject *m;
@@ -19,6 +20,8 @@ PyMODINIT_FUNC PyInit_ros_cpp(void) {
                             PyLong_FromVoidPtr((void*)(&open_bag)));
     PyObject_SetAttrString(m, "get_msg_count",
                             PyLong_FromVoidPtr((void*)(&get_msg_count)));
+    PyObject_SetAttrString(m, "get_image_dims",
+                            PyLong_FromVoidPtr((void*)(&get_image_dims)));
 
     return m;
 }
@@ -36,4 +39,15 @@ int64_t get_msg_count(rosbag::Bag* bag)
     // XXX: assuming size() always returns number of messages (undocumented)
     uint32_t num_msgs = view.size();
     return (int64_t)num_msgs;
+}
+
+void get_image_dims(int64_t* out_dims, rosbag::Bag* bag)
+{
+    rosbag::View view(*bag);
+    rosbag::MessageInstance const msg = *view.begin();
+    sensor_msgs::Image::ConstPtr im_msg = msg.instantiate<sensor_msgs::Image>();
+
+    out_dims[0] = im_msg->height;
+    out_dims[1] = im_msg->width;
+    return;
 }
--- hpat/ros.py
+++ hpat/ros.py
@@ -33,6 +33,7 @@ def _handle_read_images(lhs, rhs):
         bag = hpat.ros.open_bag(file_name)
         num_msgs = hpat.ros.get_msg_count(bag)
         m,n = hpat.ros.get_image_dims(bag)
+        # hpat.cprint(num_msgs, m, n)
         A = np.empty((num_msgs, m, n, 3), dtype=np.uint8)
         s = hpat.ros.read_ros_images_inner(A, bag)
 
@@ -40,6 +41,7 @@ def _handle_read_images(lhs, rhs):
     replace_arg_nodes(f_block, [fname])
     nodes = f_block.body[:-3]  # remove none return
     A_var = nodes[-2].value.args[0]
+    #A_var = nodes[-1].target
     nodes.append(ir.Assign(A_var, lhs, lhs.loc))
     return nodes
 
@@ -86,11 +88,13 @@ class ReadInnerTyper(AbstractTemplate):
         assert len(args) == 2
         return signature(types.int32, *args)
 
+from numba import cgutils
 from llvmlite import ir as lir
 import ros_cpp
 import llvmlite.binding as ll
 ll.add_symbol('open_bag', ros_cpp.open_bag)
 ll.add_symbol('get_msg_count', ros_cpp.get_msg_count)
+ll.add_symbol('get_image_dims', ros_cpp.get_image_dims)
 
 @lower_builtin(open_bag, hpat.string_type)
 def lower_open_bag(context, builder, sig, args):
@@ -100,8 +104,20 @@ def lower_open_bag(context, builder, sig, args):
     return builder.call(fn, args)
 
 @lower_builtin(get_msg_count, bag_file_type)
-def lower_open_bag(context, builder, sig, args):
+def lower_get_msg_count(context, builder, sig, args):
     fnty = lir.FunctionType(lir.IntType(64),
                             [lir.IntType(8).as_pointer()])
     fn = builder.module.get_or_insert_function(fnty, name="get_msg_count")
     return builder.call(fn, args)
+
+@lower_builtin(get_image_dims, bag_file_type)
+def lower_get_image_dims(context, builder, sig, args):
+    out_ptr = cgutils.alloca_once(builder, lir.IntType(64), 2)
+    out_typ = lir.ArrayType(lir.IntType(64), 2)
+    fnty = lir.FunctionType(lir.VoidType(),
+                            [lir.IntType(64).as_pointer(),
+                            lir.IntType(8).as_pointer()])
+    fn = builder.module.get_or_insert_function(fnty, name="get_image_dims")
+    builder.call(fn, [out_ptr] + args)
+    out_ptr_fixed_typ = builder.bitcast(out_ptr, out_typ.as_pointer())
+    return builder.load(out_ptr_fixed_typ)
