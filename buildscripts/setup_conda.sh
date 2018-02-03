4628d282c94441e2fe6951f09d666977182b6cdc
Ehsan Totoni <ehsan.totoni@intel.com>
2018-02-02 21:27:22 -0500

    add llvmlite on channel to travis setup

--- buildscripts/setup_conda.sh
+++ buildscripts/setup_conda.sh
@@ -25,7 +25,7 @@ conda remove --all -q -y -n $CONDA_ENV
 conda create -n $CONDA_ENV -q -y python=$PYTHON numpy=$NUMPY scipy pandas boost cmake
 source activate $CONDA_ENV
 $CONDA_INSTALL pyarrow=0.8.* mpich -c conda-forge
-$CONDA_INSTALL h5py -c ehsantn
+$CONDA_INSTALL h5py llvmlite -c ehsantn
 $CONDA_INSTALL daal-devel -c intel
 $CONDA_INSTALL tbb -c conda-forge
 
