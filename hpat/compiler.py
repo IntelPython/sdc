a65b2d0d16d1f66dca3a284b8f7288bd70581cc6
Ehsan Totoni <ehsan.totoni@intel.com>
2018-02-17 11:46:54 -0800

    autopep8 compiler.py

--- hpat/compiler.py
+++ hpat/compiler.py
@@ -14,6 +14,7 @@ from hpat import config
 if config._has_h5py:
     from hpat import pio
 
+
 def stage_io_pass(pipeline):
     """
     Convert IO calls
@@ -24,6 +25,7 @@ def stage_io_pass(pipeline):
         io_pass = pio.PIO(pipeline.func_ir, pipeline.locals)
         io_pass.run()
 
+
 def stage_distributed_pass(pipeline):
     """
     parallelize for distributed-memory
@@ -31,8 +33,9 @@ def stage_distributed_pass(pipeline):
     # Ensure we have an IR and type information.
     assert pipeline.func_ir
     dist_pass = DistributedPass(pipeline.func_ir, pipeline.typingctx,
-        pipeline.type_annotation.typemap, pipeline.type_annotation.calltypes)
+                                pipeline.type_annotation.typemap, pipeline.type_annotation.calltypes)
     dist_pass.run()
+
 
 def stage_df_pass(pipeline):
     """
@@ -40,8 +43,10 @@ def stage_df_pass(pipeline):
     """
     # Ensure we have an IR and type information.
     assert pipeline.func_ir
-    df_pass = HiFrames(pipeline.func_ir, pipeline.typingctx, pipeline.args, pipeline.locals)
+    df_pass = HiFrames(pipeline.func_ir, pipeline.typingctx,
+                       pipeline.args, pipeline.locals)
     df_pass.run()
+
 
 def stage_df_typed_pass(pipeline):
     """
@@ -50,8 +55,9 @@ def stage_df_typed_pass(pipeline):
     # Ensure we have an IR and type information.
     assert pipeline.func_ir
     df_pass = HiFramesTyped(pipeline.func_ir, pipeline.typingctx,
-        pipeline.type_annotation.typemap, pipeline.type_annotation.calltypes)
+                            pipeline.type_annotation.typemap, pipeline.type_annotation.calltypes)
     df_pass.run()
+
 
 def stage_inline_pass(pipeline):
     """
@@ -61,35 +67,45 @@ def stage_inline_pass(pipeline):
     assert pipeline.func_ir
     inline_calls(pipeline.func_ir)
 
+
 def stage_repeat_inline_closure(pipeline):
     assert pipeline.func_ir
-    inline_pass = InlineClosureCallPass(pipeline.func_ir, pipeline.flags.auto_parallel)
+    inline_pass = InlineClosureCallPass(
+        pipeline.func_ir, pipeline.flags.auto_parallel)
     inline_pass.run()
     post_proc = postproc.PostProcessor(pipeline.func_ir)
     post_proc.run()
 
+
 def add_hpat_stages(pipeline_manager, pipeline):
     pp = pipeline_manager.pipeline_stages['nopython']
     new_pp = []
-    for (func,desc) in pp:
-        if desc=='nopython frontend':
+    for (func, desc) in pp:
+        if desc == 'nopython frontend':
             # before type inference: add inline calls pass,
             # untyped hiframes pass, hdf5 io
             # also repeat inline closure pass to inline df stencils
-            new_pp.append((lambda: stage_inline_pass(pipeline), "inline funcs"))
-            new_pp.append((lambda: stage_df_pass(pipeline), "convert DataFrames"))
-            new_pp.append((lambda: stage_io_pass(pipeline), "replace IO calls"))
-            new_pp.append((lambda: stage_repeat_inline_closure(pipeline), "repeat inline closure"))
+            new_pp.append(
+                (lambda: stage_inline_pass(pipeline), "inline funcs"))
+            new_pp.append((lambda: stage_df_pass(
+                pipeline), "convert DataFrames"))
+            new_pp.append((lambda: stage_io_pass(
+                pipeline), "replace IO calls"))
+            new_pp.append((lambda: stage_repeat_inline_closure(
+                pipeline), "repeat inline closure"))
         # need to handle string array exprs before nopython rewrites converts
         # them to arrayexpr.
         # since generic_rewrites has the same description, we check func name
         if desc == 'nopython rewrites' and 'generic_rewrites' not in str(func):
-            new_pp.append((lambda: stage_df_typed_pass(pipeline), "typed hiframes pass"))
-        if desc=='nopython mode backend':
+            new_pp.append((lambda: stage_df_typed_pass(
+                pipeline), "typed hiframes pass"))
+        if desc == 'nopython mode backend':
             # distributed pass after parfor pass and before lowering
-            new_pp.append((lambda:stage_distributed_pass(pipeline), "convert to distributed"))
-        new_pp.append((func,desc))
+            new_pp.append((lambda: stage_distributed_pass(
+                pipeline), "convert to distributed"))
+        new_pp.append((func, desc))
     pipeline_manager.pipeline_stages['nopython'] = new_pp
+
 
 def inline_calls(func_ir):
     work_list = list(func_ir.blocks.items())
@@ -104,8 +120,8 @@ def inline_calls(func_ir):
                     if isinstance(func_def, ir.Global) and isinstance(func_def.value, CPUDispatcher):
                         py_func = func_def.value.py_func
                         new_blocks = inline_closure_call(func_ir,
-                                        func_ir.func_id.func.__globals__,
-                                        block, i, py_func, work_list=work_list)
+                                                         func_ir.func_id.func.__globals__,
+                                                         block, i, py_func, work_list=work_list)
                         # for block in new_blocks:
                         #     work_list.append(block)
                         # current block is modified, skip the rest
