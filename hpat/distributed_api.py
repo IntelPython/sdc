bc570dcb0dfb3c15e846383ce26c47af6bf2090d
Ehsan Totoni <ehsan.totoni@intel.com>
2018-02-17 11:45:23 -0800

    autopep8 distributed_api.py

--- hpat/distributed_api.py
+++ hpat/distributed_api.py
@@ -4,6 +4,8 @@ from numba.typing import signature
 import time
 
 from enum import Enum
+
+
 class Reduce_Type(Enum):
     Sum = 0
     Prod = 1
@@ -12,98 +14,120 @@ class Reduce_Type(Enum):
     Argmin = 4
     Argmax = 5
 
+
 def get_rank():  # pragma: no cover
     """dummy function for C mpi get_rank"""
     return 0
 
+
 def barrier():  # pragma: no cover
-    return 0;
+    return 0
+
 
 def get_size():  # pragma: no cover
     """dummy function for C mpi get_size"""
     return 0
 
+
 def get_start(total_size, pes, rank):  # pragma: no cover
     """get end point of range for parfor division"""
     return 0
+
 
 def get_end(total_size, pes, rank):  # pragma: no cover
     """get end point of range for parfor division"""
     return 0
 
+
 def get_node_portion(total_size, pes, rank):  # pragma: no cover
     """get portion of size for alloc division"""
     return 0
+
 
 def dist_reduce(value):  # pragma: no cover
     """dummy to implement simple reductions"""
     return value
 
+
 def dist_arr_reduce(arr):  # pragma: no cover
     """dummy to implement array reductions"""
     return -1
+
 
 def dist_cumsum(arr):  # pragma: no cover
     """dummy to implement cumsum"""
     return arr
 
+
 def dist_cumprod(arr):  # pragma: no cover
     """dummy to implement cumprod"""
     return arr
+
 
 def dist_exscan(value):  # pragma: no cover
     """dummy to implement simple exscan"""
     return value
 
+
 def dist_setitem(arr, index, val):  # pragma: no cover
     return 0
+
 
 def dist_time():  # pragma: no cover
     return time.time()
 
+
 def irecv():  # pragma: no cover
     return 0
+
 
 def isend():  # pragma: no cover
     return 0
 
+
 def wait():  # pragma: no cover
     return 0
+
 
 @infer_global(get_rank)
 class DistRank(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==0
+        assert len(args) == 0
         return signature(types.int32, *args)
+
 
 @infer_global(get_size)
 class DistSize(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==0
+        assert len(args) == 0
         return signature(types.int32, *args)
+
 
 @infer_global(get_start)
 class DistStart(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==3
+        assert len(args) == 3
         return signature(types.int64, *args)
+
 
 @infer_global(get_end)
 class DistEnd(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==3
+        assert len(args) == 3
         return signature(types.int64, *args)
+
 
 @infer_global(get_node_portion)
 class DistPortion(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==3
+        assert len(args) == 3
         return signature(types.int64, *args)
+
 
 @infer_global(dist_reduce)
 class DistReduce(AbstractTemplate):
@@ -112,12 +136,14 @@ class DistReduce(AbstractTemplate):
         assert len(args) == 2  # value and reduce_op
         return signature(args[0], *args)
 
+
 @infer_global(dist_exscan)
 class DistExscan(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==1
+        assert len(args) == 1
         return signature(args[0], *args)
+
 
 @infer_global(dist_arr_reduce)
 class DistArrReduce(AbstractTemplate):
@@ -126,48 +152,54 @@ class DistArrReduce(AbstractTemplate):
         assert len(args) == 2  # value and reduce_op
         return signature(types.int32, *args)
 
+
 @infer_global(time.time)
 class DistTime(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==0
+        assert len(args) == 0
         return signature(types.float64, *args)
+
 
 @infer_global(dist_time)
 class DistDistTime(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==0
+        assert len(args) == 0
         return signature(types.float64, *args)
+
 
 @infer_global(barrier)
 class DistBarrier(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==0
+        assert len(args) == 0
         return signature(types.int32, *args)
+
 
 @infer_global(dist_cumsum)
 @infer_global(dist_cumprod)
 class DistCumsumprod(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==2
+        assert len(args) == 2
         return signature(types.int32, *args)
+
 
 @infer_global(irecv)
 @infer_global(isend)
 class DistIRecv(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==5
+        assert len(args) == 5
         return signature(types.int32, *args)
+
 
 @infer_global(wait)
 class DistWait(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args)==2
+        assert len(args) == 2
         return signature(types.int32, *args)
 
 # @infer_global(dist_setitem)
