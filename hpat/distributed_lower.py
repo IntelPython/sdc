d23987fc7375c816669e35abceead6408932dcc0
Ehsan Totoni <ehsan.totoni@intel.com>
2018-02-17 11:46:05 -0800

    autopep8 distributed_lower.py

--- hpat/distributed_lower.py
+++ hpat/distributed_lower.py
@@ -33,14 +33,15 @@ ll.add_symbol('hpat_get_dummy_ptr', hdist.hpat_get_dummy_ptr)
 
 
 _h5_typ_table = {
-    types.int8:0,
-    types.uint8:1,
-    types.int32:2,
-    types.int64:3,
-    types.float32:4,
-    types.float64:5,
-    types.uint64:6
-    }
+    types.int8: 0,
+    types.uint8: 1,
+    types.int32: 2,
+    types.int64: 3,
+    types.float32: 4,
+    types.float64: 5,
+    types.uint64: 6
+}
+
 
 @lower_builtin(distributed_api.get_rank)
 def dist_get_rank(context, builder, sig, args):
@@ -48,32 +49,39 @@ def dist_get_rank(context, builder, sig, args):
     fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_get_rank")
     return builder.call(fn, [])
 
+
 @lower_builtin(distributed_api.get_size)
 def dist_get_size(context, builder, sig, args):
     fnty = lir.FunctionType(lir.IntType(32), [])
     fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_get_size")
     return builder.call(fn, [])
 
+
 @lower_builtin(distributed_api.get_start, types.int64, types.int32, types.int32)
 def dist_get_start(context, builder, sig, args):
     fnty = lir.FunctionType(lir.IntType(64), [lir.IntType(64),
-                                            lir.IntType(32), lir.IntType(32)])
-    fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_get_start")
+                                              lir.IntType(32), lir.IntType(32)])
+    fn = builder.module.get_or_insert_function(
+        fnty, name="hpat_dist_get_start")
     return builder.call(fn, [args[0], args[1], args[2]])
+
 
 @lower_builtin(distributed_api.get_end, types.int64, types.int32, types.int32)
 def dist_get_end(context, builder, sig, args):
     fnty = lir.FunctionType(lir.IntType(64), [lir.IntType(64),
-                                            lir.IntType(32), lir.IntType(32)])
+                                              lir.IntType(32), lir.IntType(32)])
     fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_get_end")
     return builder.call(fn, [args[0], args[1], args[2]])
+
 
 @lower_builtin(distributed_api.get_node_portion, types.int64, types.int32, types.int32)
 def dist_get_portion(context, builder, sig, args):
     fnty = lir.FunctionType(lir.IntType(64), [lir.IntType(64),
-                                            lir.IntType(32), lir.IntType(32)])
-    fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_get_node_portion")
+                                              lir.IntType(32), lir.IntType(32)])
+    fn = builder.module.get_or_insert_function(
+        fnty, name="hpat_dist_get_node_portion")
     return builder.call(fn, [args[0], args[1], args[2]])
+
 
 @lower_builtin(distributed_api.dist_reduce, types.int64, types.int32)
 @lower_builtin(distributed_api.dist_reduce, types.int32, types.int32)
@@ -95,7 +103,7 @@ def lower_dist_reduce(context, builder, sig, args):
             supported_typs.append(types.int64)
         if target_typ not in supported_typs:  # pragma: no cover
             raise TypeError("argmin/argmax not supported for type {}".format(
-                                                                    target_typ))
+                target_typ))
 
     in_ptr = cgutils.alloca_once(builder, val_typ)
     out_ptr = cgutils.alloca_once(builder, val_typ)
@@ -106,10 +114,10 @@ def lower_dist_reduce(context, builder, sig, args):
 
     typ_enum = _h5_typ_table[target_typ]
     typ_arg = cgutils.alloca_once_value(builder, lir.Constant(lir.IntType(32),
-                                                                    typ_enum))
+                                                              typ_enum))
 
     fnty = lir.FunctionType(lir.VoidType(), [lir.IntType(8).as_pointer(),
-                        lir.IntType(8).as_pointer(), op_typ, lir.IntType(32)])
+                                             lir.IntType(8).as_pointer(), op_typ, lir.IntType(32)])
     fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_reduce")
     builder.call(fn, [in_ptr, out_ptr, args[1], builder.load(typ_arg)])
     # cast back to value type
@@ -124,7 +132,8 @@ def lower_dist_arr_reduce(context, builder, sig, args):
 
     # store an int to specify data type
     typ_enum = _h5_typ_table[sig.args[0].dtype]
-    typ_arg = cgutils.alloca_once_value(builder, lir.Constant(lir.IntType(32), typ_enum))
+    typ_arg = cgutils.alloca_once_value(
+        builder, lir.Constant(lir.IntType(32), typ_enum))
     ndims = sig.args[0].ndim
 
     out = make_array(sig.args[0])(context, builder, args[0])
@@ -133,18 +142,21 @@ def lower_dist_arr_reduce(context, builder, sig, args):
     builder.store(out.shape, size_ptr)
     size_arg = builder.bitcast(size_ptr, lir.IntType(64).as_pointer())
 
-    ndim_arg = cgutils.alloca_once_value(builder, lir.Constant(lir.IntType(32), sig.args[0].ndim))
+    ndim_arg = cgutils.alloca_once_value(
+        builder, lir.Constant(lir.IntType(32), sig.args[0].ndim))
     call_args = [builder.bitcast(out.data, lir.IntType(8).as_pointer()),
-                size_arg, builder.load(ndim_arg), args[1], builder.load(typ_arg)]
+                 size_arg, builder.load(ndim_arg), args[1], builder.load(typ_arg)]
 
     # array, shape, ndim, extra last arg type for type enum
     arg_typs = [lir.IntType(8).as_pointer(), lir.IntType(64).as_pointer(),
-        lir.IntType(32), op_typ, lir.IntType(32)]
+                lir.IntType(32), op_typ, lir.IntType(32)]
     fnty = lir.FunctionType(lir.IntType(32), arg_typs)
-    fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_arr_reduce")
+    fn = builder.module.get_or_insert_function(
+        fnty, name="hpat_dist_arr_reduce")
     builder.call(fn, call_args)
     res = out._getvalue()
     return impl_ret_borrowed(context, builder, sig.return_type, res)
+
 
 @lower_builtin(time.time)
 def dist_get_time(context, builder, sig, args):
@@ -152,17 +164,20 @@ def dist_get_time(context, builder, sig, args):
     fn = builder.module.get_or_insert_function(fnty, name="hpat_get_time")
     return builder.call(fn, [])
 
+
 @lower_builtin(distributed_api.dist_time)
 def dist_get_dist_time(context, builder, sig, args):
     fnty = lir.FunctionType(lir.DoubleType(), [])
     fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_get_time")
     return builder.call(fn, [])
 
+
 @lower_builtin(distributed_api.barrier)
 def dist_barrier(context, builder, sig, args):
     fnty = lir.FunctionType(lir.IntType(32), [])
     fn = builder.module.get_or_insert_function(fnty, name="hpat_barrier")
     return builder.call(fn, [])
+
 
 @lower_builtin(distributed_api.dist_cumsum, types.npytypes.Array, types.npytypes.Array)
 def lower_dist_cumsum(context, builder, sig, args):
@@ -181,8 +196,8 @@ def lower_dist_cumsum(context, builder, sig, args):
         return 0
 
     res = context.compile_internal(builder, cumsum_impl, sig, args,
-                                    locals=dict(c=dtype,
-                                    prefix_var=dtype))
+                                   locals=dict(c=dtype,
+                                               prefix_var=dtype))
     return res
 
 
@@ -193,55 +208,63 @@ def lower_dist_cumsum(context, builder, sig, args):
 def lower_dist_exscan(context, builder, sig, args):
     ltyp = args[0].type
     fnty = lir.FunctionType(ltyp, [ltyp])
-    typ_map = {types.int32:"i4", types.int64:"i8", types.float32:"f4", types.float64:"f8"}
+    typ_map = {types.int32: "i4", types.int64: "i8",
+               types.float32: "f4", types.float64: "f8"}
     typ_str = typ_map[sig.args[0]]
-    fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_exscan_{}".format(typ_str))
+    fn = builder.module.get_or_insert_function(
+        fnty, name="hpat_dist_exscan_{}".format(typ_str))
     return builder.call(fn, [args[0]])
 
 
 @lower_builtin(distributed_api.irecv, types.npytypes.Array, types.int32,
-types.int32, types.int32, types.boolean)
+               types.int32, types.int32, types.boolean)
 def lower_dist_irecv(context, builder, sig, args):
     # store an int to specify data type
     typ_enum = _h5_typ_table[sig.args[0].dtype]
-    typ_arg = cgutils.alloca_once_value(builder, lir.Constant(lir.IntType(32), typ_enum))
+    typ_arg = cgutils.alloca_once_value(
+        builder, lir.Constant(lir.IntType(32), typ_enum))
 
     out = make_array(sig.args[0])(context, builder, args[0])
 
     call_args = [builder.bitcast(out.data, lir.IntType(8).as_pointer()),
-                args[1], builder.load(typ_arg),
-                args[2], args[3], args[4]]
+                 args[1], builder.load(typ_arg),
+                 args[2], args[3], args[4]]
 
     # array, size, extra arg type for type enum
     # pe, tag, cond
     arg_typs = [lir.IntType(8).as_pointer(),
-        lir.IntType(32), lir.IntType(32), lir.IntType(32), lir.IntType(32),
-        lir.IntType(1)]
+                lir.IntType(32), lir.IntType(
+                    32), lir.IntType(32), lir.IntType(32),
+                lir.IntType(1)]
     fnty = lir.FunctionType(lir.IntType(32), arg_typs)
     fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_irecv")
     return builder.call(fn, call_args)
 
+
 @lower_builtin(distributed_api.isend, types.npytypes.Array, types.int32,
-types.int32, types.int32, types.boolean)
+               types.int32, types.int32, types.boolean)
 def lower_dist_isend(context, builder, sig, args):
     # store an int to specify data type
     typ_enum = _h5_typ_table[sig.args[0].dtype]
-    typ_arg = cgutils.alloca_once_value(builder, lir.Constant(lir.IntType(32), typ_enum))
+    typ_arg = cgutils.alloca_once_value(
+        builder, lir.Constant(lir.IntType(32), typ_enum))
 
     out = make_array(sig.args[0])(context, builder, args[0])
 
     call_args = [builder.bitcast(out.data, lir.IntType(8).as_pointer()),
-                args[1], builder.load(typ_arg),
-                args[2], args[3], args[4]]
+                 args[1], builder.load(typ_arg),
+                 args[2], args[3], args[4]]
 
     # array, size, extra arg type for type enum
     # pe, tag, cond
     arg_typs = [lir.IntType(8).as_pointer(),
-        lir.IntType(32), lir.IntType(32), lir.IntType(32), lir.IntType(32),
-        lir.IntType(1)]
+                lir.IntType(32), lir.IntType(
+                    32), lir.IntType(32), lir.IntType(32),
+                lir.IntType(1)]
     fnty = lir.FunctionType(lir.IntType(32), arg_typs)
     fn = builder.module.get_or_insert_function(fnty, name="hpat_dist_isend")
     return builder.call(fn, call_args)
+
 
 @lower_builtin(distributed_api.wait, types.int32, types.boolean)
 def lower_dist_wait(context, builder, sig, args):
@@ -291,6 +314,8 @@ def lower_dist_wait(context, builder, sig, args):
 # find overlapping range of an input range (start:stop) and a chunk range
 # (chunk_start:chunk_start+chunk_count). Inputs are assumed positive.
 # output is set to empty range of local range goes out of bounds
+
+
 @numba.njit
 def _get_local_range(start, stop, chunk_start, chunk_count):  # pragma: no cover
     assert start >= 0 and stop > 0
@@ -303,10 +328,12 @@ def _get_local_range(start, stop, chunk_start, chunk_count):  # pragma: no cover
         loc_stop = 0
     return loc_start, loc_stop
 
+
 @numba.njit
 def _set_if_in_range(A, val, index, chunk_start, chunk_count):  # pragma: no cover
-    if index >= chunk_start and index < chunk_start+chunk_count:
+    if index >= chunk_start and index < chunk_start + chunk_count:
         A[index] = val
+
 
 @numba.njit
 def _root_rank_select(old_val, new_val):  # pragma: no cover
