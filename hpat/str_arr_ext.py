9521a7e2aaca4874fffd6866388bd19dbbad687b
Ehsan Totoni <ehsan.totoni@intel.com>
2018-03-13 14:56:41 -0400

    typeof register ndarray of strings

--- hpat/str_arr_ext.py
+++ hpat/str_arr_ext.py
@@ -326,12 +326,17 @@ def lower_string_arr_getitem(context, builder, sig, args):
 
 
 import pandas
+import numpy as np
 
 @typeof_impl.register(pandas.Series)
 def typeof_pd_str_series(val, c):
     if isinstance(val[0], str):  # and isinstance(val[-1], str):
         return string_array_type
 
+@typeof_impl.register(np.ndarray)
+def typeof_np_string(val, c):
+    if val.ndim == 1 and isinstance(val[0], str):  # and isinstance(val[-1], str):
+        return string_array_type
 
 @unbox(StringArrayType)
 def unbox_str(typ, val, c):
