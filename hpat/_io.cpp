7d62fc45384270442f194f39692ae639ea812573
Ehsan Totoni <ehsan.totoni@intel.com>
2018-03-19 09:55:53 -0700

    debug MPI I/O large files

--- hpat/_io.cpp
+++ hpat/_io.cpp
@@ -330,6 +330,11 @@ void file_read(std::string* file_name, void* buff, int64_t size)
 
 void file_read_parallel(std::string* file_name, char* buff, int64_t start, int64_t count)
 {
+    // printf("MPI READ %lld %lld\n", start, count);
+    char err_string[MPI_MAX_ERROR_STRING];
+    err_string[MPI_MAX_ERROR_STRING-1] = '\0';
+    int err_len, err_class;
+    MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
 
     MPI_File fh;
     int ierr = MPI_File_open(MPI_COMM_WORLD, (const char*)file_name->c_str(),
@@ -342,22 +347,39 @@ void file_read_parallel(std::string* file_name, char* buff, int64_t start, int64
         #define LARGE_DTYPE_SIZE 1024
         MPI_Datatype large_dtype;
         MPI_Type_contiguous(LARGE_DTYPE_SIZE, MPI_CHAR, &large_dtype);
+        MPI_Type_commit(&large_dtype);
         int read_size = (int) (count/LARGE_DTYPE_SIZE);
 
         ierr = MPI_File_read_at_all(fh, (MPI_Offset)start, buff,
                              read_size, large_dtype, MPI_STATUS_IGNORE);
-        if (ierr!=0) std::cerr << "File read error: " << *file_name << '\n';
+        if (ierr!=0)
+        {
+            MPI_Error_class(ierr, &err_class);
+            std::cerr << "File large read error: " << err_class << " " << *file_name << '\n';
+            MPI_Error_string(ierr, err_string, &err_len);
+            printf("Error %s\n", err_string); fflush(stdout);
+        }
+        MPI_Type_free(&large_dtype);
         int64_t left_over = count % LARGE_DTYPE_SIZE;
         int64_t read_byte_size = count-left_over;
+        // printf("VAL leftover %lld read %lld\n", left_over, read_byte_size);
         start += read_byte_size;
         buff += read_byte_size;
-        count = read_byte_size;
+        count = left_over;
     }
+    // printf("MPI leftover READ %lld %lld\n", start, count);
 
     ierr = MPI_File_read_at_all(fh, (MPI_Offset)start, buff,
                          (int)count, MPI_CHAR, MPI_STATUS_IGNORE);
 
-    if (ierr!=0) std::cerr << "File read error: " << *file_name << '\n';
+    // if (ierr!=0) std::cerr << "File read error: " << *file_name << '\n';
+    if (ierr!=0)
+    {
+        MPI_Error_class(ierr, &err_class);
+        std::cerr << "File read error: " << err_class << " " << *file_name << '\n';
+        MPI_Error_string(ierr, err_string, &err_len);
+        printf("Error %s\n", err_string); fflush(stdout);
+    }
 
     MPI_File_close(&fh);
     return;
