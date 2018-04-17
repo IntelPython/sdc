b84576598810b7c7afffbc4251ec77251a1cc5c3
Ehsan Totoni <ehsan.totoni@intel.com>
2018-04-16 21:36:44 -0400

    fix parquet reader Windows build

--- buildscripts/parquet-reader-conda-recipe/build.sh
+++ buildscripts/parquet-reader-conda-recipe/build.sh
@@ -2,7 +2,7 @@ mkdir -p build
 pushd build
 rm -rf *
 cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DPREFIX=$PREFIX ..
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DPQ_PREFIX=$PREFIX ..
 make VERBOSE=1
 make install
 popd
