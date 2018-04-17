8ec66633c44fcb2a7cee78aafe83d8e4a53508e4
Ehsan Totoni <ehsan.totoni@intel.com>
2018-04-17 08:21:32 -0400

    update h5py and hpat buildscripts

--- buildscripts/hpat-conda-recipe/build.sh
+++ buildscripts/hpat-conda-recipe/build.sh
@@ -1,4 +1,4 @@
 # LDSHARED="mpicxx -cxx=$GXX -shared" LD="mpicxx -cxx=$GXX" \
 # CC="mpicxx -cxx=$GXX -std=c++11" GXX="mpicxx -cxx=$GXX -std=c++11" \
-DAALROOT="${PREFIX}" HDF5_DIR="${PREFIX}" MACOSX_DEPLOYMENT_TARGET=10.9 \
+OPENCV_DIR="${PREFIX}" DAALROOT="${PREFIX}" HDF5_DIR="${PREFIX}" MACOSX_DEPLOYMENT_TARGET=10.9 \
 $PYTHON setup.py build install --single-version-externally-managed --record=record.txt
