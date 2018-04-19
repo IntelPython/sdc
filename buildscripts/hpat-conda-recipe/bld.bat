911552f3ae9a4b6b05970ccdffc1629ad972ba54
Ehsan Totoni <ehsan.totoni@intel.com>
2018-04-18 20:34:58 -0400

    update recipes to add host, remove daal and pq reader for win

--- buildscripts/hpat-conda-recipe/bld.bat
+++ buildscripts/hpat-conda-recipe/bld.bat
@@ -1,4 +1,4 @@
 set HDF5_DIR="%LIBRARY_PREFIX%"
-set DAALROOT="%LIBRARY_PREFIX%"
+REM set DAALROOT="%LIBRARY_PREFIX%"
 "%PYTHON%" setup.py build install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
