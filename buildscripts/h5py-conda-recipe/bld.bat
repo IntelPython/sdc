e57940996d14481d701e3523a6316f7a675b8691
Ehsan Totoni <ehsan.totoni@intel.com>
2018-04-18 20:31:50 -0400

    update h5py hdf5 version to 1.10.1

--- buildscripts/h5py-conda-recipe/bld.bat
+++ buildscripts/h5py-conda-recipe/bld.bat
@@ -1,4 +1,4 @@
-"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%" --hdf5-version=1.8.19
+"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%" --hdf5-version=1.10.1
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py build install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
