95fc2cdb31abe3e729f867e82b15f57d088cf67b
Ehsan Totoni <ehsan.totoni@intel.com>
2018-06-07 08:29:31 -0400

    parquet-reader build CONDA_PREFIX

--- buildscripts/parquet-reader-conda-recipe/build.sh
+++ buildscripts/parquet-reader-conda-recipe/build.sh
@@ -2,7 +2,7 @@ mkdir -p build
 pushd build
 rm -rf *
 cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DPQ_PREFIX=$PREFIX $SRC_DIR
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DPQ_PREFIX=$CONDA_PREFIX $SRC_DIR
 make VERBOSE=1
 make install
 popd
