0a53650ce705c5fbee249280575240c06b80dff5
Ehsan Totoni <ehsan.totoni@intel.com>
2018-07-20 13:43:30 -0400

    series tests

---
+++ hpat/tests/test_series.py
@@ -0,0 +1,77 @@
+import unittest
+import pandas as pd
+import numpy as np
+import random
+import string
+import pyarrow.parquet as pq
+import numba
+import hpat
+from hpat import hiframes_sort
+from hpat.str_arr_ext import StringArray
+from hpat.tests.test_utils import (count_array_REPs, count_parfor_REPs,
+                            count_parfor_OneDs, count_array_OneDs, dist_IR_contains)
+
+class TestSeries(unittest.TestCase):
+    def test_create1(self):
+        def test_impl():
+            df = pd.DataFrame({'A': [1,2,3]})
+            return (df.A == 1).sum()
+
+        hpat_func = hpat.jit(test_impl)
+        self.assertEqual(hpat_func(), test_impl())
+
+    def test_create2(self):
+        def test_impl(n):
+            df = pd.DataFrame({'A': np.arange(n)})
+            return (df.A == 2).sum()
+
+        n = 11
+        hpat_func = hpat.jit(test_impl)
+        self.assertEqual(hpat_func(n), test_impl(n))
+
+    def test_create_str(self):
+        def test_impl():
+            df = pd.DataFrame({'A': ['a', 'b', 'c']})
+            return (df.A == 'a').sum()
+
+        hpat_func = hpat.jit(test_impl)
+        self.assertEqual(hpat_func(), test_impl())
+
+    def test_pass_df1(self):
+        def test_impl(df):
+            return (df.A == 2).sum()
+
+        n = 11
+        df = pd.DataFrame({'A': np.arange(n)})
+        hpat_func = hpat.jit(test_impl)
+        self.assertEqual(hpat_func(df), test_impl(df))
+
+    def test_pass_df_str(self):
+        def test_impl(df):
+            return (df.A == 'a').sum()
+
+        df = pd.DataFrame({'A': ['a', 'b', 'c']})
+        hpat_func = hpat.jit(test_impl)
+        self.assertEqual(hpat_func(df), test_impl(df))
+
+    def test_pass_series1(self):
+        # TODO: check to make sure it is series type
+        def test_impl(A):
+            return (A == 2).sum()
+
+        n = 11
+        df = pd.DataFrame({'A': np.arange(n)})
+        hpat_func = hpat.jit(test_impl)
+        self.assertEqual(hpat_func(df.A), test_impl(df.A))
+
+    def test_pass_series_str(self):
+        def test_impl(A):
+            return (A == 'a').sum()
+
+        df = pd.DataFrame({'A': ['a', 'b', 'c']})
+        hpat_func = hpat.jit(test_impl)
+        self.assertEqual(hpat_func(df.A), test_impl(df.A))
+
+
+if __name__ == "__main__":
+    unittest.main()
