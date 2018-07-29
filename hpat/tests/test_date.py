44ce1e2f5002458f369b647451be1a554f3cbdfe
Ehsan Totoni <ehsan.totoni@intel.com>
2018-07-29 16:59:24 -0400

    refactor pd.DatetimeIndex test

--- hpat/tests/test_date.py
+++ hpat/tests/test_date.py
@@ -15,16 +15,20 @@ import random
 class TestDate(unittest.TestCase):
     def test_datetime_index(self):
         def test_impl(df):
-            df['hpat'] = pd.DatetimeIndex(df['orig'])
+            return pd.DatetimeIndex(df['str_date']).values
 
         hpat_func = hpat.jit(test_impl)
-        rows = 10
-        data = []
-        for row in range(rows):
-            data.append(datetime(2017, random.randint(1,12), random.randint(1,28)).isoformat())
-        df = pd.DataFrame({'orig' : data})
+        df = self._gen_str_date_df()
+        np.testing.assert_array_equal(hpat_func(df), test_impl(df))
+
+    def test_datetime_index_set(self):
+        def test_impl(df):
+            df['hpat'] = pd.DatetimeIndex(df['str_date']).values
+
+        hpat_func = hpat.jit(test_impl)
+        df = self._gen_str_date_df()
         hpat_func(df)
-        df['std'] = pd.DatetimeIndex(df['orig'])
+        df['std'] = pd.DatetimeIndex(df['str_date'])
         allequal = (df['std'].equals(df['hpat']))
         self.assertTrue(allequal)
 
@@ -52,6 +56,13 @@ class TestDate(unittest.TestCase):
         df = pd.DataFrame({'A': pd.DatetimeIndex(['2015-01-03', '2010-10-11'])})
         hpat_func = hpat.jit(test_impl)
         np.testing.assert_array_equal(hpat_func(df), test_impl(df))
+
+    def _gen_str_date_df(self):
+        rows = 10
+        data = []
+        for row in range(rows):
+            data.append(datetime(2017, random.randint(1,12), random.randint(1,28)).isoformat())
+        return pd.DataFrame({'str_date' : data})
 
 if __name__ == "__main__":
     unittest.main()
