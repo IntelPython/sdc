306f0bbb8f3c5b65bf4190a36551bd2bb4722548
Ehsan Totoni <ehsan.totoni@intel.com>
2018-08-22 11:41:54 -0400

    disable daal import errors

--- hpat/ml/__init__.py
+++ hpat/ml/__init__.py
@@ -1,6 +1,9 @@
 from .svc import SVC
 from .naive_bayes import MultinomialNB
+from hpat.utils import debug_prints
+
 try:
     from . import d4p
 except ImportError:
-    print("daal4py import error")
+    if debug_prints():  # pragma: no cover
+        print("daal4py import error")
