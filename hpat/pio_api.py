5d0d2ceb98c721ac86e845f2bfd5992a5134f02f
Ehsan Totoni <ehsan.totoni@intel.com>
2018-08-27 15:56:34 -0400

    h5 dset/group types, file getitem infer

--- hpat/pio_api.py
+++ hpat/pio_api.py
@@ -2,16 +2,38 @@ from numba import types
 from numba.typing.templates import infer_global, AbstractTemplate, AttributeTemplate, bound_function
 from numba.typing import signature
 import h5py
-from numba.extending import register_model, models, infer_getattr
+from numba.extending import register_model, models, infer_getattr, infer
 from hpat.str_ext import string_type
 
+
+################## Types #######################
 
 class H5FileType(types.Opaque):
     def __init__(self):
         super(H5FileType, self).__init__(name='H5FileType')
 
-
 h5file_type = H5FileType()
+
+
+class H5DatasetType(types.Opaque):
+    def __init__(self):
+        super(H5DatasetType, self).__init__(name='H5DatasetType')
+
+h5dataset_type = H5DatasetType()
+
+
+class H5GroupType(types.Opaque):
+    def __init__(self):
+        super(H5GroupType, self).__init__(name='H5GroupType')
+
+h5group_type = H5GroupType()
+
+
+class H5DatasetOrGroupType(types.Opaque):
+    def __init__(self):
+        super(H5DatasetOrGroupType, self).__init__(name='H5DatasetOrGroupType')
+
+h5dataset_or_group_type = H5DatasetOrGroupType()
 
 h5file_data_type = types.int64
 
@@ -21,17 +43,21 @@ if h5py.version.hdf5_version_tuple[1] == 8:
 else:
     assert h5py.version.hdf5_version_tuple[1] == 10
 
-# TODO: create similar types for groups and datasets
-
 
 @register_model(H5FileType)
+@register_model(H5DatasetType)
+@register_model(H5GroupType)
+@register_model(H5DatasetOrGroupType)
 class H5FileModel(models.IntegerModel):
     def __init__(self, dmm, fe_type):
         super(H5FileModel, self).__init__(dmm, h5file_data_type)
 
 
 # type for list of names
-string_list_type = types.containers.List(string_type)
+string_list_type = types.List(string_type)
+
+
+#################################################
 
 
 @infer_getattr
@@ -43,6 +69,17 @@ class FileAttribute(AttributeTemplate):
         assert not kws
         assert not args
         return signature(string_list_type, *args)
+
+@infer
+class GetItemH5File(AbstractTemplate):
+    key = "getitem"
+
+    def generic(self, args, kws):
+        assert not kws
+        (in_f, in_idx) = args
+        if in_f == h5file_type:
+            assert in_idx == string_type
+            return signature(h5dataset_or_group_type, in_f, in_idx)
 
 
 def h5g_get_num_objs():
@@ -87,7 +124,6 @@ def h5write():
 class H5File(AbstractTemplate):
     def generic(self, args, kws):
         assert not kws
-        assert len(args) == 3
         return signature(h5file_type, *args)
 
 
