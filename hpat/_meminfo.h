ece9a67142bbf96d204822d9924b205143af91d6
Ehsan Totoni <ehsan.totoni@intel.com>
2018-08-28 15:17:43 -0400

    proper checks in importing meminfo func

--- hpat/_meminfo.h
+++ hpat/_meminfo.h
@@ -8,22 +8,24 @@
  */
 static void *
 import_meminfo_func(const char * func) {
+#define CHECK(expr, msg) if(!(expr)){std::cerr << msg << std::endl; PyGILState_Release(gilstate); return NULL;}
     PyObject *nrtmod = NULL;
     PyObject *helperdct = NULL;
     PyObject *mi_rel_fn = NULL;
     void *fnptr = NULL;
+    auto gilstate = PyGILState_Ensure();
     /* from numba.runtime import _nrt_python */
     nrtmod = PyImport_ImportModule("numba.runtime._nrt_python");
-    if (!nrtmod) goto cleanup;
+    CHECK(nrtmod, "importing numba.runtime._nrt_python failed");
     /* helperdct = _nrt_python.c_helpers */
     helperdct = PyObject_GetAttrString(nrtmod, "c_helpers");
-    if (!helperdct) goto cleanup;
+    CHECK(helperdct, "getting numba.runtime._nrt_python.c_helpers failed");
     /* helperdct[func] */
     mi_rel_fn = PyDict_GetItemString(helperdct, func);
-    if (!mi_rel_fn) goto cleanup;
+    CHECK(mi_rel_fn, "getting meminfo func failed");
     fnptr = PyLong_AsVoidPtr(mi_rel_fn);
 
-cleanup:
+    PyGILState_Release(gilstate);
     Py_XDECREF(nrtmod);
     Py_XDECREF(helperdct);
     return fnptr;
