90752e1c73e6619b9871232274235ea451d71ce2
Ehsan Totoni <ehsan.totoni@intel.com>
2018-08-31 16:42:05 -0400

    version bump 0.24

--- setup.py
+++ setup.py
@@ -258,7 +258,7 @@ if _has_xenon:
     _ext_mods.append(ext_xenon_wrapper)
 
 setup(name='hpat',
-      version='0.23.3',
+      version='0.24.0',
       description='compiling Python code for clusters',
       long_description=readme(),
       classifiers=[
