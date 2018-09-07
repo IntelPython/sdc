671137d5b403706ed4abaded57ddee557e36063c
Ehsan Totoni <ehsan.totoni@intel.com>
2018-09-07 09:31:30 -0400

    df values tests

--- hpat/tests/test_hiframes.py
+++ hpat/tests/test_hiframes.py
@@ -34,6 +34,24 @@ class TestHiFrames(unittest.TestCase):
         self.assertEqual(count_parfor_REPs(), 0)
         self.assertEqual(count_parfor_OneDs(), 1)
 
+    def test_df_values1(self):
+        def test_impl(n):
+            df = pd.DataFrame({'A': np.ones(n), 'B': np.arange(n)})
+            return df.values
+
+        hpat_func = hpat.jit(test_impl)
+        n = 11
+        np.testing.assert_array_equal(hpat_func(n), test_impl(n))
+
+    def test_df_values2(self):
+        def test_impl(df):
+            return df.values
+
+        hpat_func = hpat.jit(test_impl)
+        n = 11
+        df = pd.DataFrame({'A': np.ones(n), 'B': np.arange(n)})
+        np.testing.assert_array_equal(hpat_func(df), test_impl(df))
+
     def test_df_box(self):
         def test_impl(n):
             df = pd.DataFrame({'A': np.ones(n), 'B': np.arange(n)})
