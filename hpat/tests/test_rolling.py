2058e2032d6053fa3309eb6794a0e1c18d038dfa
Ehsan Totoni <ehsan.totoni@intel.com>
2018-09-11 10:10:13 -0400

    rolling fixed apply generated seq test

--- hpat/tests/test_rolling.py
+++ hpat/tests/test_rolling.py
@@ -56,6 +56,18 @@ class TestRolling(unittest.TestCase):
                 df = pd.DataFrame({'B': [0, 1, 2, -2, 4]})
                 pd.testing.assert_frame_equal(hpat_func(df, *args), test_impl(df, *args))
 
+    def test_fixed_apply2(self):
+        # test sequentially with generated dfs
+        def test_impl(df, w, c):
+            return df.rolling(w, center=c).apply(lambda a: a.sum())
+        hpat_func = hpat.jit(test_impl)
+        sizes = (1, 2, 10, 11, 121, 1000)
+        wins = (2, 3, 5)
+        centers = (False, True)
+        for n, w, c in itertools.product(sizes, wins, centers):
+            df = pd.DataFrame({'B': np.arange(n)})
+            pd.testing.assert_frame_equal(hpat_func(df, w, c), test_impl(df, w, c))
+
     def test_fixed_parallel1(self):
         def test_impl(n, w, center):
             df = pd.DataFrame({'B': np.arange(n)})
