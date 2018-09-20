341479d816c70926df9e40886ba616535d8de08e
Ehsan Totoni <ehsan.totoni@intel.com>
2018-09-20 08:33:09 -0400

    support agg var seq

--- hpat/hiframes_aggregate.py
+++ hpat/hiframes_aggregate.py
@@ -40,11 +40,13 @@ AggFuncStruct = namedtuple('AggFuncStruct',
      'eval_all_func'])
 
 supported_agg_funcs = ['sum', 'count', 'mean',
-                       'min', 'max', 'prod', 'agg', 'aggregate']
+                       'min', 'max', 'prod', 'var', 'agg', 'aggregate']
 
 
 def get_agg_func(func_ir, func_name, rhs):
     from hpat.hiframes_typed import series_replace_funcs
+    if func_name == 'var':
+        return _column_var_impl_linear
     if func_name in supported_agg_funcs[:-2]:
         return series_replace_funcs[func_name]
 
@@ -64,6 +66,19 @@ def get_agg_func(func_ir, func_name, rhs):
     agg_func = agg_func_wrapper
     return agg_func
 
+def _column_var_impl_linear(A):  # pragma: no cover
+    nobs = 0
+    mean_x = 0.0
+    ssqdm_x = 0.0
+    N = len(A)
+    for i in numba.parfor.internal_prange(N):
+        val = A[i]
+        if not np.isnan(val):
+            nobs += 1
+            delta = val - mean_x
+            mean_x += delta / nobs
+            ssqdm_x += ((nobs - 1) * delta ** 2) / nobs
+    return hpat.hiframes_rolling.calc_var(2, nobs, mean_x, ssqdm_x)
 
 
 class Aggregate(ir.Stmt):
--- hpat/tests/test_hiframes.py
+++ hpat/tests/test_hiframes.py
@@ -800,6 +800,15 @@ class TestHiFrames(unittest.TestCase):
         df = pd.DataFrame({'A': [2,1,1,1,2,2,1], 'B': [-8,2,3,1,5,6,7]})
         self.assertEqual(set(hpat_func(df)), set(test_impl(df)))
 
+    def test_agg_seq_var(self):
+        def test_impl(df):
+            A = df.groupby('A')['B'].var()
+            return A.values
+
+        hpat_func = hpat.jit(test_impl)
+        df = pd.DataFrame({'A': [2,1,1,1,2,2,1], 'B': [-8,2,3,1,5,6,7]})
+        self.assertEqual(set(hpat_func(df)), set(test_impl(df)))
+
     def test_agg_parallel(self):
         def test_impl(n):
             df = pd.DataFrame({'A': np.ones(n, np.int64), 'B': np.arange(n)})
@@ -864,6 +873,19 @@ class TestHiFrames(unittest.TestCase):
         def test_impl(n):
             df = pd.DataFrame({'A': np.ones(n, np.int64), 'B': np.arange(n)})
             A = df.groupby('A')['B'].max()
+            return A.sum()
+
+        hpat_func = hpat.jit(test_impl)
+        n = 11
+        self.assertEqual(hpat_func(n), test_impl(n))
+        self.assertEqual(count_array_REPs(), 0)
+        self.assertEqual(count_parfor_REPs(), 0)
+
+    @unittest.skip("pending reduction dep fix")
+    def test_agg_parallel_var(self):
+        def test_impl(n):
+            df = pd.DataFrame({'A': np.ones(n, np.int64), 'B': np.arange(n)})
+            A = df.groupby('A')['B'].var()
             return A.sum()
 
         hpat_func = hpat.jit(test_impl)
