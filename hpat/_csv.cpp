03e25d8f9249e72d333a36b3baeab05fec4a2fdd
Frank Schlimbach <frank.schlimbach@intel.com>
2018-09-25 12:41:39 -0500

    fixing decls

--- hpat/_csv.cpp
+++ hpat/_csv.cpp
@@ -14,6 +14,7 @@
 
 #include "_datetime_ext.h"
 #include "_distributed.h"
+#include "_csv.h"
 
 #include <Python.h>
 #include "structmember.h"
@@ -196,32 +197,6 @@ void PyInit_csv(PyObject * m)
 #define ALIGN 32
 
 extern "C" {
-
-/**
-   Read a CSV file.
-   Returned pointer must be deallocated by calling csv_delete using the same n_cols_to_read.
-   All lines have the same number of tokens/columns.
-   Otherwise some lines might be duplicated or skipped in a distributed setup.
-   Input parameters must be identical on all ranks, dead-lock can occur otherwise.
-
-   @param[in] fname           Name of the CSV file
-   @param[in] cols_to_read    Array of column indices (size_t) to be read
-                              If NULL, all columns will be read
-   @param[in] dtypes          Array of data type codes (from numpy)
-                              Expects array of size n_cols_to_read.
-                              Must not be NULL.
-   @param[in] n_cols_to_read  Number of columns to read.
-   @param[out] first_row_read On success (if data is returned) this will be set to first line number read by this process.
-   @param[out] n_rows_read    On success (if data is returned) this will be set to number of lines read by this process.
-   @param[in]  delimiters     Any character in the string is considered to be a separator.
-   @param[in]  quotes         Any character in the string is considered to be a quote.
-   @return 2d-array: array of n_cols_to_read arrays, each of given data type (dtypes)
-                     array of NULL pointers if no data was read
-                     NULL if an error ocurred.
- **/
-MemInfo ** csv_read_file(const std::string * fname, size_t * cols_to_read, int64_t * dtypes, size_t n_cols_to_read,
-                         size_t * first_row, size_t * n_rows,
-                         std::string * delimiters = NULL, std::string * quotes = NULL);
 /**
    Like csv_read_file, but reading from a string.
  **/
@@ -512,16 +487,16 @@ static MemInfo** csv_read(std::istream & f, size_t fsz, size_t * cols_to_read, i
 }
 #endif
 
-extern "C" MemInfo ** csv_read_file(const std::string * fname, size_t * cols_to_read, int64_t * dtypes, size_t n_cols_to_read,
-                                    size_t * first_row, size_t * n_rows,
-                                    std::string * delimiters, std::string * quotes)
+extern "C" void ** csv_read_file(const std::string * fname, size_t * cols_to_read, int64_t * dtypes, size_t n_cols_to_read,
+                                 size_t * first_row, size_t * n_rows,
+                                 std::string * delimiters, std::string * quotes)
 {
     CHECK(fname != NULL, "NULL filename provided.");
     // get total file-size
     auto fsz = boost::filesystem::file_size(*fname);
     std::ifstream f(*fname);
     CHECK(f.good() && !f.eof() && f.is_open(), "could not open file.");
-    return csv_read(f, fsz, cols_to_read, dtypes, n_cols_to_read, first_row, n_rows, delimiters, quotes);
+    return reinterpret_cast<void**>(csv_read(f, fsz, cols_to_read, dtypes, n_cols_to_read, first_row, n_rows, delimiters, quotes));
 }
 
 
--- hpat/_csv.h
+++ hpat/_csv.h
@@ -7,8 +7,30 @@
 // CSV exports some stuff to the io module
 extern void PyInit_csv(PyObject *);
 
-extern void ** csv_read_file(const std::string * fname, size_t * cols_to_read, int * dtypes, size_t n_cols_to_read,
-                             size_t * first_row, size_t * n_rows,
-                             std::string * delimiters, std::string * quotes);
+/**
+   Read a CSV file.
+   Returned pointer must be deallocated by calling csv_delete using the same n_cols_to_read.
+   All lines have the same number of tokens/columns.
+   Otherwise some lines might be duplicated or skipped in a distributed setup.
+   Input parameters must be identical on all ranks, dead-lock can occur otherwise.
+
+   @param[in] fname           Name of the CSV file
+   @param[in] cols_to_read    Array of column indices (size_t) to be read
+                              If NULL, all columns will be read
+   @param[in] dtypes          Array of data type codes (from numpy)
+                              Expects array of size n_cols_to_read.
+                              Must not be NULL.
+   @param[in] n_cols_to_read  Number of columns to read.
+   @param[out] first_row_read On success (if data is returned) this will be set to first line number read by this process.
+   @param[out] n_rows_read    On success (if data is returned) this will be set to number of lines read by this process.
+   @param[in]  delimiters     Any character in the string is considered to be a separator.
+   @param[in]  quotes         Any character in the string is considered to be a quote.
+   @return 2d-array: array of n_cols_to_read arrays, each of given data type (dtypes)
+                     array of NULL pointers if no data was read
+                     NULL if an error ocurred.
+ **/
+extern "C" void ** csv_read_file(const std::string * fname, size_t * cols_to_read, int64_t * dtypes, size_t n_cols_to_read,
+                                 size_t * first_row, size_t * n_rows,
+                                 std::string * delimiters, std::string * quotes);
 
 #endif // _CSV_H_INCLUDED
