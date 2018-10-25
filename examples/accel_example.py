6a280c0b732a7b7e55190391cc935bcb7a5844e7
Ehsan Totoni <ehsan.totoni@intel.com>
2018-10-25 16:13:58 -0400

    add accel example

---
+++ examples/accel_example.py
@@ -0,0 +1,29 @@
+import numpy as np
+import time
+import pandas as pd
+import hpat
+
+
+@hpat.jit
+def accel_infer(n):
+
+    t1 = time.time()
+    X = np.random.ranf(n)
+    Y = np.random.ranf(n)
+    Z = np.random.ranf(n)
+
+    df = pd.DataFrame({'X': X, 'Y': Y, 'Z': Z})
+
+    g = 9.81
+    df['accel'] = np.sqrt(df.X**2 + df.Y**2 + (df.Z-g)**2)
+    threshold = df.accel.mean() + 5 * df.accel.std()
+    df['is_brake'] = (df.rolling(10)['accel'].mean() > threshold)
+
+    df.is_brake.fillna(False, inplace=True)
+    checksum = df.is_brake.sum()
+    t2 = time.time()
+    print("exec time:", t2-t1)
+    return checksum
+
+n = 10**8
+accel_infer(n)
