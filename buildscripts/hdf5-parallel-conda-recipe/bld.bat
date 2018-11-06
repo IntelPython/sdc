cb2c5e50665aea7b95c43af5645edf93f5351572
Ehsan Totoni <ehsan.totoni@intel.com>
2018-11-05 21:01:20 -0800

    hdf5 recipe update to 1.10.4 (add conda-forge flags etc)

--- buildscripts/hdf5-parallel-conda-recipe/bld.bat
+++ buildscripts/hdf5-parallel-conda-recipe/bld.bat
@@ -5,23 +5,27 @@ cd build
 :: Set environment variables.
 set HDF5_EXT_ZLIB=zlib.lib
 
+set "CXXFLAGS=%CXXFLAGS% -LTCG"
+
 :: Configure step.
-cmake -G "NMake Makefiles" ^
+cmake -G "Ninja" ^
       -D CMAKE_BUILD_TYPE:STRING=RELEASE ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
       -D BUILD_SHARED_LIBS:BOOL=ON ^
       -D HDF5_BUILD_HL_LIB=ON ^
       -D HDF5_BUILD_TOOLS:BOOL=ON ^
+      -D HDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON ^
       -D ALLOW_UNSUPPORTED=ON ^
       -D HDF5_ENABLE_PARALLEL:BOOL=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
 :: Build C libraries and tools.
-nmake
+ninja
 if errorlevel 1 exit 1
 
 :: Install step.
-nmake install
+ninja install
 if errorlevel 1 exit 1
