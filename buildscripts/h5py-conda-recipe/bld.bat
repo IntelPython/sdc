37a7d4eae55e8676147d5fbb418e7e369c6c4199
Ehsan Totoni <ehsan.totoni@intel.com>
2018-11-06 06:26:29 -0800

    update h5py recipe

--- buildscripts/h5py-conda-recipe/bld.bat
+++ buildscripts/h5py-conda-recipe/bld.bat
@@ -1,4 +1,5 @@
-"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%" --hdf5-version=1.10.1
+"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%" --hdf5-version=%hdf5%
 if errorlevel 1 exit 1
-"%PYTHON%" setup.py build install --single-version-externally-managed --record=record.txt
+
+"%PYTHON%" -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 if errorlevel 1 exit 1
