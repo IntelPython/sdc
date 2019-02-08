994dae2ab71f961e82d0a87104266b5ac54e0601
Ehsan Totoni <ehsan.totoni@intel.com>
2019-02-08 12:14:25 -0500

    fix str flatten with new Series

--- hpat/tests/test_hiframes.py
+++ hpat/tests/test_hiframes.py
@@ -801,7 +801,7 @@ class TestHiFrames(unittest.TestCase):
                     'B:return':'distributed'})(test_impl)
         pd.testing.assert_series_equal(
             hpat_func(df), test_impl(df), check_names=False)
-        self.assertEqual(count_array_REPs(), 3)
+        self.assertEqual(count_array_REPs(), 4)
         self.assertEqual(count_parfor_REPs(), 0)
 
     def test_to_numeric(self):
