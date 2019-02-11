167ea12c1f6eb795d21ff60555aa2b1fe56c3676
Ehsan Totoni <ehsan.totoni@intel.com>
2019-02-11 08:54:02 -0500

    refactor jit decorator and exports

---
+++ hpat/decorators.py
@@ -0,0 +1,35 @@
+import numba
+import hpat
+
+
+def jit(signature_or_function=None, **options):
+    # set nopython by default
+    if 'nopython' not in options:
+        options['nopython'] = True
+
+    _locals = options.pop('locals', {})
+
+    # put pivots in locals TODO: generalize numba.jit options
+    pivots = options.pop('pivots', {})
+    for var, vals in pivots.items():
+        _locals[var+":pivot"] = vals
+
+    h5_types = options.pop('h5_types', {})
+    for var, vals in h5_types.items():
+        _locals[var+":h5_types"] = vals
+
+    options['locals'] = _locals
+
+    #options['parallel'] = True
+    options['parallel'] = {'comprehension': True,
+                           'setitem':       False,  # FIXME: support parallel setitem
+                           'reduction':     True,
+                           'numpy':         True,
+                           'stencil':       True,
+                           'fusion':        True,
+                           }
+
+    # this is for previous version of pipeline manipulation (numba hpat_req <0.38)
+    # from .compiler import add_hpat_stages
+    # return numba.jit(signature_or_function, user_pipeline_funcs=[add_hpat_stages], **options)
+    return numba.jit(signature_or_function, pipeline_class=hpat.compiler.HPATPipeline, **options)
