60a1b93b8852aff263bdf4ff66cf2a4c869a6a06
Frank Schlimbach <frank.schlimbach@intel.com>
2019-03-01 07:07:45 -0600

    allow call-analysis from external modules, removing daal4py features (now in daal4py)

--- hpat/ml/__init__.py
+++ hpat/ml/__init__.py
@@ -1,9 +1,3 @@
 from .svc import SVC
 from .naive_bayes import MultinomialNB
 from hpat.utils import debug_prints
-
-try:
-    from . import d4p
-except ImportError:
-    if debug_prints():  # pragma: no cover
-        print("daal4py import error")
