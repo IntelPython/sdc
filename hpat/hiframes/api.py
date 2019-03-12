473c87f51b9c13e511d05b196cbb5a01021aba3f
Ehsan Totoni <ehsan.totoni@intel.com>
2019-03-12 15:24:18 -0400

    series dropna fix

--- hpat/hiframes/api.py
+++ hpat/hiframes/api.py
@@ -370,7 +370,7 @@ class DropNAType(AbstractTemplate):
         assert not kws
         assert len(args) in (1, 2)
         # args: in_arr
-        return signature(args[0], *args)
+        return signature(if_arr_to_series_type(args[0]), *args)
 
 @infer_global(column_sum)
 class SumType(AbstractTemplate):
