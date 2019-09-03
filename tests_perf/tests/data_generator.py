1d8f59d3bfb6aca555af3152be7fcd89e31101b6
Denis <denis.smirnov@intel.com>
2019-09-03 16:25:43 +0300

    Add perf test case for writing to CSV

--- tests_perf/tests/data_generator.py
+++ tests_perf/tests/data_generator.py
@@ -10,17 +10,35 @@ class DataGenerator:
         """Generate one random unicode string."""
         return tm.randu(length)
 
+    def range(self, length, repeat=1):
+        """Generate range with int32 values."""
+        return np.arange(length).repeat(repeat)
+
+    def randn(self, length, repeat=1):
+        """Generate random array with float64 values."""
+        return randn(length).repeat(repeat)
+
     def make_int_series(self, length, repeat=1, index=None, name=None):
         """Generate series with float range."""
-        data = pd.Int64Index(np.arange(length)).repeat(repeat)
+        data = pd.Int64Index(self.range(length, repeat=repeat))
         return pd.Series(data, index=index, name=name)
 
     def make_uint_series(self, length, repeat=1, index=None, name=None):
         """Generate series with unsigned integers range."""
-        data = pd.UInt64Index(np.arange(length)).repeat(repeat)
+        data = pd.UInt64Index(self.range(length, repeat=repeat))
         return pd.Series(data, index=index, name=name)
 
     def make_float_series(self, length, repeat=1, index=None, name=None):
-        """Generate series with random integers."""
-        data = pd.Float64Index(randn(length)).repeat(repeat)
+        """Generate series with random floats."""
+        data = pd.Float64Index(self.randn(length, repeat=repeat))
         return pd.Series(data, index=index, name=name)
+
+    def make_numeric_dataframe(self, length, repeat=1, index=None):
+        """Generate simple numeric data frame with integers/floats ranges."""
+        data = self.range(length, repeat=repeat)
+        return pd.DataFrame({
+            'A': data,
+            'B': data + 1.0,
+            'C': data + 2.0,
+            'D': data + 3.0,
+        }, index=index)
