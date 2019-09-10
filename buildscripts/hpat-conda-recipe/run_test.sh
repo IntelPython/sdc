6006aaa77befd8ed53705ac31a6f28c6bb7a467b
Vyacheslav Smirnov <vyacheslav.s.smirnov@intel.com>
2019-09-10 18:01:38 +0300

    Make Travis use conda-build
     - Make Travis use conda-build
     - Add build for pure python without MPI (using HPAT_CONFIG_MPI var)
     - Add python version define
     - Move coverage to run_test.sh
     - Set numba version to 0.46.0 and pandas to 0.25.0

--- buildscripts/hpat-conda-recipe/run_test.sh
+++ buildscripts/hpat-conda-recipe/run_test.sh
@@ -8,6 +8,15 @@ export PYTHONFAULTHANDLER=1
 
 python -m hpat.tests.gen_test_data
 
-python -u -m hpat.runtests -v
-mpiexec -n 2 python -u -m hpat.runtests -v
-mpiexec -n 3 python -u -m hpat.runtests -v
+if [ "$HPAT_RUN_COVERAGE" == "True" ]; then
+  export PYTHONPATH=.
+  coverage erase
+  coverage run --source=./hpat --omit ./hpat/ml/*,./hpat/xenon_ext.py,./hpat/ros.py,./hpat/cv_ext.py,./hpat/tests/* -W ignore -u -m hpat.runtests -v
+else
+  # TODO investigate root cause of NumbaPerformanceWarning
+  if [ -z "$HPAT_NUM_PES" ]; then
+    mpiexec -n $HPAT_NUM_PES python -W ignore -u -m hpat.runtests -v
+  else
+    python -W ignore -u -m hpat.runtests -v
+  fi
+fi
