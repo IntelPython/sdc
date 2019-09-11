cea9a57af97c717c3341f7875cbf62116bdc8104
Vyacheslav Smirnov <vyacheslav.s.smirnov@intel.com>
2019-09-11 13:22:02 +0300

    Set numba to recent stable version
    (cherry picked from commit 53e1d5f00a78d2d1f5c66bb85354c4fa79956b72)

--- buildscripts/hpat-conda-recipe/run_test.sh
+++ buildscripts/hpat-conda-recipe/run_test.sh
@@ -11,9 +11,10 @@ python -m hpat.tests.gen_test_data
 if [ "$HPAT_RUN_COVERAGE" == "True" ]; then
   export PYTHONPATH=.
   coverage erase
-  coverage run --source=./hpat --omit ./hpat/ml/*,./hpat/xenon_ext.py,./hpat/ros.py,./hpat/cv_ext.py,./hpat/tests/* -W ignore -u -m hpat.runtests -v
+  coverage run --source=./hpat --omit ./hpat/ml/*,./hpat/xenon_ext.py,./hpat/ros.py,./hpat/cv_ext.py,./hpat/tests/* -u -m hpat.runtests -v
 else
   # TODO investigate root cause of NumbaPerformanceWarning
+  # http://numba.pydata.org/numba-doc/latest/user/parallel.html#diagnostics
   if [ -z "$HPAT_NUM_PES" ]; then
     python -W ignore -u -m hpat.runtests -v
   else
