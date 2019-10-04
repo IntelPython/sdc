51951cd97a31904aa2fab7add20cf2cc72c6445a
spokhode <sergey.pokhodenko@intel.com>
2019-10-04 12:58:18 +0300

    enable test_sort_parallel_single_col

--- hpat/tests/test_dataframe.py
+++ hpat/tests/test_dataframe.py
@@ -567,7 +567,6 @@ class TestDataFrame(unittest.TestCase):
         hpat_func = hpat.jit(test_impl)
         self.assertTrue((hpat_func(df) == sorted_df.B.values).all())
 
-    @unittest.skip('Error - fix needed; issue is related to __pycache__\n')
     def test_sort_parallel_single_col(self):
         # create `kde.parquet` file
         ParquetGenerator.gen_kde_pq()
