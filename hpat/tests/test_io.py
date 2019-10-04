4cfc919fda6dcc1d63399894564f68bbbbf8af6f
spokhode <sergey.pokhodenko@intel.com>
2019-10-04 13:14:35 +0300

    enable test_pq_read_global_str1

--- hpat/tests/test_io.py
+++ hpat/tests/test_io.py
@@ -209,11 +209,6 @@ class TestIO(unittest.TestCase):
         self.assertEqual(count_array_REPs(), 0)
         self.assertEqual(count_parfor_REPs(), 0)
 
-    @unittest.skip('AssertionError - fix needed\n'
-                   'Arrays are not almost equal to 7 decimals\n'
-                   'ACTUAL: 59.92340551591986\n'
-                   'DESIRED: 58.34405719897534\n'
-                   'NUMA_PES=3 build')
     def test_pq_read_global_str1(self):
         def test_impl():
             df = pd.read_parquet(kde_file)
