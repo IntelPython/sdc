b1b281f4fe6a61c26188a222e513aab72e38531d
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2019-10-20 03:48:45 +0300

    Implement Series.mean() in new style (#219)

--- hpat/hiframes/pd_series_ext.py
+++ hpat/hiframes/pd_series_ext.py
@@ -992,7 +992,7 @@ for fname in ["cumsum", "cumprod"]:
 
 # TODO: add itemsize, strides, etc. when removed from Pandas
 _not_series_array_attrs = ['flat', 'ctypes', 'itemset', 'reshape', 'sort', 'flatten',
-                           'resolve_shift', 'resolve_sum', 'resolve_copy',
+                           'resolve_shift', 'resolve_sum', 'resolve_copy', 'resolve_mean',
                            'resolve_take', 'resolve_max', 'resolve_min', 'resolve_nunique']
 
 # use ArrayAttribute for attributes not defined in SeriesAttribute
