2b5a3901df226044584c00cc471228bced1d421d
Vyacheslav Smirnov <vyacheslav.s.smirnov@intel.com>
2019-10-23 19:29:14 +0300

    Change HPAT_WHEELS_DIR variable setting for Windows

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -3,4 +3,4 @@ set -ex
 
 source activate $CONDA_ENV
 
-conda build --python $PYTHON_VER --numpy=$NUMPY_VER -c numba -c https://metachannel.conda-forge.org/conda-forge/python,setuptools,numpy,pandas,pyarrow,arrow-cpp,boost,hdf5,h5py,mpich,wheel,coveralls,pip -c defaults --override-channels ./buildscripts/hpat-conda-recipe/
+conda build --python $PYTHON_VER --numpy=$NUMPY_VER -c numba -c conda-forge -c defaults --override-channels ./buildscripts/hpat-conda-recipe/
