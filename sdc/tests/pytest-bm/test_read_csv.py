93530dfc90be181bb28bfe676c2cae788922a0f2
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2019-11-20 13:05:21 +0300

    Add Python tests for PyArrow and pandas_read_csv() via PyArrow

--- sdc/tests/pytest-bm/test_read_csv.py
+++ sdc/tests/pytest-bm/test_read_csv.py
@@ -1,5 +1,6 @@
 import sdc
 import pandas
+import pyarrow.csv
 
 
 def pandas_read_csv():
@@ -10,11 +11,39 @@ def pandas_read_csv():
     return df
 
 
-def test_pandas_read_csv(benchmark):
+def pyarrow_read_csv():
+    """
+    pyarrow.csv.read_csv()
+    """
+    df = pyarrow.csv.read_csv('data_100000.csv').to_pandas()
+    return df
+
+
+def pandas_read_csv_via_pyarrow():
+    """
+    pandas_read_csv()
+    """
+    df = sdc.io.csv_ext.pandas_read_csv('data_100000.csv')
+    return df
+
+
+def test_py_pandas_read_csv(benchmark):
     result = benchmark(pandas_read_csv)
 
     # no need to compare with itself
     # pandas.testing.assert_frame_equal(result, pandas_read_csv())
+
+
+def test_py_pyarrow_read_csv(benchmark):
+    result = benchmark(pyarrow_read_csv)
+
+    pandas.testing.assert_frame_equal(result, pandas_read_csv())
+
+
+def test_py_pandas_read_csv_via_pyarrow(benchmark):
+    result = benchmark(pandas_read_csv_via_pyarrow)
+
+    pandas.testing.assert_frame_equal(result, pandas_read_csv())
 
 
 def test_sdc_pandas_read_csv(benchmark):
