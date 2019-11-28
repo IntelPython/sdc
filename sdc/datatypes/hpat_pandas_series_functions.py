da5707f795110c5012a3ce9634c102cfc0b82418
mrubtsov <maria.rubtsova@intel.com>
2019-11-28 17:29:25 +0300

    fix corr

--- sdc/datatypes/hpat_pandas_series_functions.py
+++ sdc/datatypes/hpat_pandas_series_functions.py
@@ -1200,7 +1200,20 @@ def hpat_pandas_series_corr(self, other, method='pearson', min_periods=None):
         if len(self_arr) < min_periods:
             return numpy.nan
 
-        return numpy.corrcoef(self_arr, other_arr)[0, 1]
+        new_self = pandas.Series(self_arr)
+        new_other = pandas.Series(other_arr)
+
+        n = new_self.count()
+        ma = new_self.sum()
+        mb = new_other.sum()
+        a = n * (self_arr * other_arr).sum() - ma * mb
+        b1 = n * (self_arr * self_arr).sum() - ma * ma
+        b2 = n * (other_arr * other_arr).sum() - mb * mb
+
+        if b1 == 0 or b2 == 0:
+            return numpy.nan
+
+        return a / numpy.sqrt(b1 * b2)
 
     return hpat_pandas_series_corr_impl
 
