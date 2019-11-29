bf8c0238f400c989fc1e9749c0ba975247a14f68
elena.totmenina <etotmeni@nntsatel03.inn.intel.com>
2019-11-29 12:49:58 +0300

    Init dataframe

--- sdc/datatypes/hpat_pandas_dataframe_functions.py
+++ sdc/datatypes/hpat_pandas_dataframe_functions.py
@@ -31,64 +31,316 @@
 
 import operator
 import pandas
+import numpy
+
+import sdc
 
 from numba import types
 from numba.extending import (overload, overload_method, overload_attribute)
+from sdc.hiframes.pd_dataframe_ext import DataFrameType
 from numba.errors import TypingError
 
-from sdc.datatypes.hpat_pandas_dataframe_types import DataFrameType
+if not sdc.config.use_default_dataframe:
+    from sdc.datatypes.hpat_pandas_dataframe_types import DataFrameType
 
 
-@overload_method(DataFrameType, 'count')
-def sdc_pandas_dataframe_count(self, axis=0, level=None, numeric_only=False):
-    """
-    Pandas DataFrame method :meth:`pandas.DataFrame.count` implementation.
+    @overload_method(DataFrameType, 'count')
+    def sdc_pandas_dataframe_count(self, axis=0, level=None, numeric_only=False):
+        """
+        Pandas DataFrame method :meth:`pandas.DataFrame.count` implementation.
+        .. only:: developer
+            Test: python -m sdc.runtests sdc.tests.test_dataframe.TestDataFrame.test_count
+        Parameters
+        -----------
+        self: :class:`pandas.DataFrame`
+            input arg
+        axis:
+            *unsupported*
+        level:
+            *unsupported*
+        numeric_only:
+            *unsupported*
+        Returns
+        -------
+        :obj:`pandas.Series` or `pandas.DataFrame`
+                returns: For each column/row the number of non-NA/null entries. If level is specified returns a DataFrame.
+        """
 
-    .. only:: developer
+        _func_name = 'Method pandas.dataframe.count().'
 
-        Test: python -m sdc.runtests sdc.tests.test_dataframe.TestDataFrame.test_count
+        if not isinstance(self, DataFrameType):
+            raise TypingError('{} The object must be a pandas.dataframe. Given: {}'.format(_func_name, self))
 
-    Parameters
-    -----------
-    self: :class:`pandas.DataFrame`
-        input arg
-    axis:
-        *unsupported*
-    level:
-        *unsupported*
-    numeric_only:
-        *unsupported*
+        if not (isinstance(axis, types.Omitted) or axis == 0):
+            raise TypingError("{} 'axis' unsupported. Given: {}".format(_func_name, axis))
 
-    Returns
-    -------
-    :obj:`pandas.Series` or `pandas.DataFrame`
-            returns: For each column/row the number of non-NA/null entries. If level is specified returns a DataFrame.
-    """
+        if not (isinstance(level, types.Omitted) or level is None):
+            raise TypingError("{} 'level' unsupported. Given: {}".format(_func_name, axis))
 
-    _func_name = 'Method pandas.dataframe.count().'
+        if not (isinstance(numeric_only, types.Omitted) or numeric_only is False):
+            raise TypingError("{} 'numeric_only' unsupported. Given: {}".format(_func_name, axis))
 
-    if not isinstance(self, DataFrameType):
-        raise TypingError('{} The object must be a pandas.dataframe. Given: {}'.format(_func_name, self))
+        def sdc_pandas_dataframe_count_impl(self, axis=0, level=None, numeric_only=False):
+            result_data = []
+            result_index = []
 
-    if not (isinstance(axis, types.Omitted) or axis == 0):
-        raise TypingError("{} 'axis' unsupported. Given: {}".format(_func_name, axis))
+            for dataframe_item in self._data:
+                item_count = dataframe_item.count()
+                item_name = dataframe_item._name
+                result_data.append(item_count)
+                result_index.append(item_name)
 
-    if not (isinstance(level, types.Omitted) or level is None):
-        raise TypingError("{} 'level' unsupported. Given: {}".format(_func_name, axis))
+            return pandas.Series(data=result_data, index=result_index)
 
-    if not (isinstance(numeric_only, types.Omitted) or numeric_only is False):
-        raise TypingError("{} 'numeric_only' unsupported. Given: {}".format(_func_name, axis))
+        return sdc_pandas_dataframe_count_impl
 
-    def sdc_pandas_dataframe_count_impl(self, axis=0, level=None, numeric_only=False):
-        result_data = []
-        result_index = []
+else:
+    def reduce(df, name):
+        saved_columns = df.columns
+        n_cols = len(saved_columns)
+        data_args = tuple('data{}'.format(i) for i in range(n_cols))
+        func_text = "def _reduce_impl(df, axis=None, skipna=None, level=None, numeric_only=None):\n"
+        for i, d in enumerate(data_args):
+            func_text += "  {} = hpat.hiframes.api.init_series(hpat.hiframes.pd_dataframe_ext.get_dataframe_data(df, {}))\n".format(
+                d + '_S', i)
+            func_text += "  {} = {}.{}()\n".format(d + '_O', d + '_S', name)
+        func_text += "  data = np.array(({},))\n".format(
+            ", ".join(d + '_O' for d in data_args))
+        func_text += "  index = hpat.str_arr_ext.StringArray(({},))\n".format(
+            ", ".join("'{}'".format(c) for c in saved_columns))
+        func_text += "  return hpat.hiframes.api.init_series(data, index)\n"
+        loc_vars = {}
 
-        for dataframe_item in self._data:
-            item_count = dataframe_item.count()
-            item_name = dataframe_item._name
-            result_data.append(item_count)
-            result_index.append(item_name)
+        print()
+        print(func_text)
+        print()
 
-        return pandas.Series(data=result_data, index=result_index)
+        exec(func_text, {'hpat': sdc, 'np': numpy}, loc_vars)
+        _reduce_impl = loc_vars['_reduce_impl']
 
-    return sdc_pandas_dataframe_count_impl
+        return _reduce_impl
+
+
+    @overload_method(DataFrameType, 'median')
+    def median_overload(df, axis=None, skipna=None, level=None, numeric_only=None):
+        """
+           Pandas DataFrame method :meth:`pandas.DataFrame.median` implementation.
+           .. only:: developer
+               Test: python -m sdc.runtests sdc.tests.test_dataframe.TestDataFrame.test_median1
+           Parameters
+           -----------
+           self: :class:`pandas.DataFrame`
+               input arg
+           axis:
+                *unsupported*
+           skipna:
+               *unsupported*
+           level:
+               *unsupported*
+           numeric_only:
+               *unsupported*
+           Returns
+           -------
+           :obj:`pandas.Series` or `pandas.DataFrame`
+                   return the median of the values for the requested axis.
+           """
+
+        name = 'median'
+
+        if not isinstance(df, DataFrameType):
+            raise TypingError('{} The object must be a pandas.dataframe. Given: {}'.format(name, df))
+
+        if not (isinstance(axis, types.Omitted) or axis is None):
+            raise TypingError("{} 'axis' unsupported. Given: {}".format(name, axis))
+
+        if not (isinstance(skipna, types.Omitted) or skipna is None):
+            raise TypingError("{} 'skipna' unsupported. Given: {}".format(name, skipna))
+
+        if not (isinstance(level, types.Omitted) or level is None):
+            raise TypingError("{} 'level' unsupported. Given: {}".format(name, level))
+
+        if not (isinstance(numeric_only, types.Omitted) or numeric_only is None):
+            raise TypingError("{} 'numeric_only' unsupported. Given: {}".format(name, numeric_only))
+
+        return reduce(df, name)
+
+
+    @overload_method(DataFrameType, 'mean')
+    def mean_overload(df, axis=None, skipna=None, level=None, numeric_only=None):
+        """
+           Pandas DataFrame method :meth:`pandas.DataFrame.mean` implementation.
+           .. only:: developer
+               Test: python -m sdc.runtests sdc.tests.test_dataframe.TestDataFrame.test_mean1
+           Parameters
+           -----------
+           self: :class:`pandas.DataFrame`
+               input arg
+           axis:
+                *unsupported*
+           skipna:
+               *unsupported*
+           level:
+               *unsupported*
+           numeric_only:
+               *unsupported*
+           Returns
+           -------
+           :obj:`pandas.Series` or `pandas.DataFrame`
+                   return the mean of the values for the requested axis.
+           """
+
+        name = 'mean'
+
+        if not isinstance(df, DataFrameType):
+            raise TypingError('{} The object must be a pandas.dataframe. Given: {}'.format(name, df))
+
+        if not (isinstance(axis, types.Omitted) or axis is None):
+            raise TypingError("{} 'axis' unsupported. Given: {}".format(name, axis))
+
+        if not (isinstance(skipna, types.Omitted) or skipna is None):
+            raise TypingError("{} 'skipna' unsupported. Given: {}".format(name, skipna))
+
+        if not (isinstance(level, types.Omitted) or level is None):
+            raise TypingError("{} 'level' unsupported. Given: {}".format(name, level))
+
+        if not (isinstance(numeric_only, types.Omitted) or numeric_only is None):
+            raise TypingError("{} 'numeric_only' unsupported. Given: {}".format(name, numeric_only))
+
+        return reduce(df, name)
+
+
+    @overload_method(DataFrameType, 'max')
+    def max_overload(df, axis=None, skipna=None, level=None, numeric_only=None):
+        """
+           Pandas DataFrame method :meth:`pandas.DataFrame.max` implementation.
+           .. only:: developer
+               Test: python -m sdc.runtests sdc.tests.test_dataframe.TestDataFrame.test_max1
+           Parameters
+           -----------
+           self: :class:`pandas.DataFrame`
+               input arg
+           axis:
+                *unsupported*
+           skipna:
+               *unsupported*
+           level:
+               *unsupported*
+           numeric_only:
+               *unsupported*
+           Returns
+           -------
+           :obj:`pandas.Series` or `pandas.DataFrame`
+                   return the maximum of the values for the requested axis.
+           """
+
+        name = 'max'
+
+        if not isinstance(df, DataFrameType):
+            raise TypingError('{} The object must be a pandas.dataframe. Given: {}'.format(name, df))
+
+        if not (isinstance(axis, types.Omitted) or axis is None):
+            raise TypingError("{} 'axis' unsupported. Given: {}".format(name, axis))
+
+        if not (isinstance(skipna, types.Omitted) or skipna is None):
+            raise TypingError("{} 'skipna' unsupported. Given: {}".format(name, skipna))
+
+        if not (isinstance(level, types.Omitted) or level is None):
+            raise TypingError("{} 'level' unsupported. Given: {}".format(name, level))
+
+        if not (isinstance(numeric_only, types.Omitted) or numeric_only is None):
+            raise TypingError("{} 'numeric_only' unsupported. Given: {}".format(name, numeric_only))
+
+        return reduce(df, name)
+
+
+    @overload_method(DataFrameType, 'min')
+    def min_overload(df, axis=None, skipna=None, level=None, numeric_only=None):
+        """
+           Pandas DataFrame method :meth:`pandas.DataFrame.min` implementation.
+           .. only:: developer
+               Test: python -m sdc.runtests sdc.tests.test_dataframe.TestDataFrame.test_min1
+           Parameters
+           -----------
+           self: :class:`pandas.DataFrame`
+               input arg
+           axis:
+                *unsupported*
+           skipna:
+               *unsupported*
+           level:
+               *unsupported*
+           numeric_only:
+               *unsupported*
+           Returns
+           -------
+           :obj:`pandas.Series` or `pandas.DataFrame`
+                   returns: the minimum of the values for the requested axis.
+           """
+
+        name = 'min'
+        
+        if not isinstance(df, DataFrameType):
+            raise TypingError('{} The object must be a pandas.dataframe. Given: {}'.format(name, df))
+
+        if not (isinstance(axis, types.Omitted) or axis is None):
+            raise TypingError("{} 'axis' unsupported. Given: {}".format(name, axis))
+
+        if not (isinstance(skipna, types.Omitted) or skipna is None):
+            raise TypingError("{} 'skipna' unsupported. Given: {}".format(name, skipna))
+
+        if not (isinstance(level, types.Omitted) or level is None):
+            raise TypingError("{} 'level' unsupported. Given: {}".format(name, level))
+
+        if not (isinstance(numeric_only, types.Omitted) or numeric_only is None):
+            raise TypingError("{} 'numeric_only' unsupported. Given: {}".format(name, numeric_only))
+        
+        return reduce(df, name)
+
+
+    @overload_method(DataFrameType, 'sum')
+    def sum_overload(df, axis=None, skipna=None, level=None, numeric_only=None, min_count=0):
+        """
+           Pandas DataFrame method :meth:`pandas.DataFrame.sum` implementation.
+           .. only:: developer
+               Test: python -m sdc.runtests sdc.tests.test_dataframe.TestDataFrame.test_sum1
+           Parameters
+           -----------
+           self: :class:`pandas.DataFrame`
+               input arg
+           axis:
+                *unsupported*
+           skipna:
+               *unsupported*
+           level:
+               *unsupported*
+           numeric_only:
+               *unsupported*
+           min_count:
+                *unsupported*
+           Returns
+           -------
+           :obj:`pandas.Series` or `pandas.DataFrame`
+                   return the sum of the values for the requested axis.
+           """
+
+        name = 'sum'
+
+        if not isinstance(df, DataFrameType):
+            raise TypingError('{} The object must be a pandas.dataframe. Given: {}'.format(name, df))
+
+        if not (isinstance(axis, types.Omitted) or axis is None):
+            raise TypingError("{} 'axis' unsupported. Given: {}".format(name, axis))
+
+        if not (isinstance(skipna, types.Omitted) or skipna is None):
+            raise TypingError("{} 'skipna' unsupported. Given: {}".format(name, skipna))
+
+        if not (isinstance(level, types.Omitted) or level is None):
+            raise TypingError("{} 'level' unsupported. Given: {}".format(name, level))
+
+        if not (isinstance(numeric_only, types.Omitted) or numeric_only is None):
+            raise TypingError("{} 'numeric_only' unsupported. Given: {}".format(name, numeric_only))
+
+        if not (isinstance(min_count, types.Omitted) or min_count == 0):
+            raise TypingError("{} 'min_count' unsupported. Given: {}".format(name, min_count))
+
+        return reduce(df, name)
