76d588da96041e46beac568602534129bc5475b8
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2019-12-11 13:42:00 +0300

    Make PyArrow cpu_count to correspond NUMBA_NUM_THREADS (#349)
    * Make PyArrow cpu_count to correspond NUMBA_NUM_THREADS
    
    Add pyarrow_cpu_count context manager which always returns cpu_count to previous value.
    
    * Improve benchmark test for read_csv()
    
    Use single config data in all places
    Add PyArrow benchmark record
    read_csv data size (1m,10)
    Show size [rows,cols]
    Implement data file caching and move functions for generating to gen_csv.py
    
    * Use functools.wraps
    
    * Use contextlib.contextmanager

--- sdc/tests/tests_perf/test_perf_read_csv.py
+++ sdc/tests/tests_perf/test_perf_read_csv.py
@@ -26,32 +26,15 @@
 # *****************************************************************************
 
 import time
-import random
 
 import pandas
+import pyarrow.csv
 import sdc
 
-from .test_perf_base import TestBase
-from .test_perf_utils import calc_compilation, get_times
+from sdc.tests.tests_perf.test_perf_base import TestBase
+from sdc.tests.tests_perf.test_perf_utils import calc_compilation, get_times
 
-from .gen_csv import generate
-
-
-def generate_csv():
-    """Generate CSV file and return file name."""
-    rows = 10**5
-    file_name = f"data_{rows}.csv"
-    r = random.Random(0)  # seed=0
-    generate(rows,
-        ['A', 'B', 'C'],
-        [
-            lambda: int(r.random() * 10000),
-            lambda: r.uniform(-1.0, 1.0),
-            lambda: r.uniform(-1.0, 1.0)
-        ],
-        file_name
-    )
-    return file_name
+from sdc.tests.tests_perf.gen_csv import generate_csv
 
 
 def make_func(file_name):
@@ -65,12 +48,23 @@ def make_func(file_name):
     return _function
 
 
+def make_func_pyarrow(file_name):
+    """Create function implemented via PyArrow."""
+    def _function():
+        start = time.time()
+        df = sdc.io.csv_ext.pandas_read_csv(file_name)
+        return time.time() - start, df
+    return _function
+
+
 class TestPandasReadCSV(TestBase):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.generated_file = generate_csv()
+        cls.rows = 10**6
+        cls.columns = 10
+        cls.generated_file = generate_csv(cls.rows, cls.columns)
 
     def _test_jitted(self, pyfunc, record, *args, **kwargs):
         # compilation time
@@ -92,7 +86,7 @@ class TestPandasReadCSV(TestBase):
     def _test_case(self, pyfunc, name):
         base = {
             "test_name": name,
-            "data_size": 10**5,
+            "data_size": f"[{self.rows},{self.columns}]",
         }
 
         record = base.copy()
@@ -107,3 +101,23 @@ class TestPandasReadCSV(TestBase):
 
     def test_read_csv(self):
         self._test_case(make_func(self.generated_file), 'read_csv')
+
+    def test_read_csv_pyarrow(self):
+        pyfunc = make_func_pyarrow(self.generated_file)
+        name = 'read_csv'
+
+        base = {
+            "test_name": name,
+            "data_size": f"[{self.rows},{self.columns}]",
+        }
+
+        record = base.copy()
+        record["test_type"] = 'PyArrow'
+        self._test_python(pyfunc, record)
+        self.test_results.add(**record)
+
+
+if __name__ == "__main__":
+    print("Gererate data files...")
+    generate_csv(rows=10**6, columns=10)
+    print("Data files generated!")
