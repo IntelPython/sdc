2cec5e4a06f3e4973440bab0cd3f8ad856931515
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-01-16 10:00:44 +0300

    Clean imports

--- sdc/tests/tests_perf/test_perf_df.py
+++ sdc/tests/tests_perf/test_perf_df.py
@@ -25,7 +25,6 @@
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # *****************************************************************************
 
-import pandas as pd
 import time
 
 import pandas
