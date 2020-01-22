ae75a25c89930d7a268c4e0bf508b904cc713657
Denis <denis.smirnov@intel.com>
2020-01-22 19:12:51 +0300

    Add perf.test for df.rolling.quantile()

--- sdc/tests/tests_perf/test_perf_df_rolling.py
+++ sdc/tests/tests_perf/test_perf_df_rolling.py
@@ -84,6 +84,7 @@ class TestDFRollingMethods(TestBase):
         super().setUpClass()
         cls.total_data_length = {
             'min': [2 * 10 ** 5],
+            'quantile': [2 * 10 ** 5],
         }
 
     def _test_jitted(self, pyfunc, record, *args, **kwargs):
@@ -135,3 +136,6 @@ class TestDFRollingMethods(TestBase):
 
     def test_df_rolling_min(self):
         self._test_df_rolling_method('min')
+
+    def test_df_rolling_quantile(self):
+        self._test_df_rolling_method('quantile', method_params='0.25')
