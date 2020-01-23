2a003efac90d3b0f7448fb1918e0d720ddb641bb
mrubtsov <maria.rubtsova@intel.com>
2020-01-23 19:43:29 +0300

    combination of two functions

--- sdc/tests/tests_perf/generator.py
+++ sdc/tests/tests_perf/generator.py
@@ -19,17 +19,19 @@ def gen(cases, method, class_add, typ, prefix=''):
         func_name = to_varname('test_{}_{}_{}'.format(typ, prefix, name))
 
         setattr(class_add, func_name, method(test_params.name, test_params.params,
-                                             test_params.size, test_params.call_expression, prefix))
+                                             test_params.size, prefix, test_params.call_expression))
 
 
-def test_gen(name, params, data_length, call_expression, prefix):
+def test_gen(name, params, data_length, prefix, call_expression):
     func_name = 'func'
     if call_expression is None:
         call_expression = '{}{}({})'.format(prefix, name, params)
+    input_data = 'input_data'
+    function_called = '{}.{}'.format(input_data, call_expression)
 
     func_text = f"""\
 def {func_name}(self):
-  self._test_case(usecase_gen('{call_expression}'), '{name}', {data_length})
+  self._test_case(usecase_gen('{input_data}', '{function_called}'), '{name}', {data_length})
 """
 
     global_vars = {'usecase_gen': usecase_gen}
@@ -41,34 +43,17 @@ def {func_name}(self):
     return func
 
 
-def usecase_gen(call_expression):
+def test_gen_two_par(name, params, data_length, prefix, *args, **kwargs):
     func_name = 'func'
-
-    func_text = f"""\
-def {func_name}(input_data):
-  start_time = time.time()
-  res = input_data.{call_expression}
-  finish_time = time.time()
-  return finish_time - start_time, res
-"""
-
-    loc_vars = {}
-    exec(func_text, globals(), loc_vars)
-    func = loc_vars[func_name]
-
-    return func
-
-
-def test_gen_two_par(name, params, data_length, *args, **kwargs):
-    func_name = 'func'
+    input_data = 'A, B'
+    function_called = 'A.{}{}(B, {})'.format(prefix, name, params)
 
     func_text = f"""\
 def {func_name}(self):
-  self._test_binary_operations(usecase_gen_two_par('{name}', '{params}'),
-                                  '{name}', {data_length})
+  self._test_binary_operations(usecase_gen('{input_data}', '{function_called}'), '{name}', {data_length})
 """
 
-    global_vars = {'usecase_gen_two_par': usecase_gen_two_par}
+    global_vars = {'usecase_gen': usecase_gen}
 
     loc_vars = {}
     exec(func_text, global_vars, loc_vars)
@@ -77,13 +62,13 @@ def {func_name}(self):
     return func
 
 
-def usecase_gen_two_par(name, par):
+def usecase_gen(input_data, function_called):
     func_name = 'func'
 
     func_text = f"""\
-def {func_name}(A, B):
+def {func_name}({input_data}):
   start_time = time.time()
-  res = A.{name}(B, {par})
+  res = {function_called}
   finish_time = time.time()
   return finish_time - start_time, res
 """
