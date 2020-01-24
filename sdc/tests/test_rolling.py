58f2858714fa0eacf572adf21faae003ee08b534
densmirn <53897164+densmirn@users.noreply.github.com>
2020-01-24 17:11:56 +0300

    Overload df.rolling.kurt() (#475)
    * Overload df.rolling.kurt()
    
    * Add perf.test for df.rolling.kurt()

--- sdc/tests/test_rolling.py
+++ sdc/tests/test_rolling.py
@@ -583,6 +583,21 @@ class TestRolling(TestCase):
                     ref_result = test_impl(obj, window, min_periods)
                     assert_equal(jit_result, ref_result)
 
+    def _test_rolling_kurt(self, obj):
+        def test_impl(obj, window, min_periods):
+            return obj.rolling(window, min_periods).kurt()
+
+        hpat_func = self.jit(test_impl)
+        assert_equal = self._get_assert_equal(obj)
+
+        for window in range(4, len(obj) + 1):
+            for min_periods in range(window + 1):
+                with self.subTest(obj=obj, window=window,
+                                  min_periods=min_periods):
+                    ref_result = test_impl(obj, window, min_periods)
+                    jit_result = hpat_func(obj, window, min_periods)
+                    assert_equal(jit_result, ref_result)
+
     def _test_rolling_max(self, obj):
         def test_impl(obj, window, min_periods):
             return obj.rolling(window, min_periods).max()
@@ -676,6 +691,15 @@ class TestRolling(TestCase):
         df = pd.DataFrame(data)
 
         self._test_rolling_count(df)
+
+    @skip_sdc_jit('DataFrame.rolling.kurt() unsupported')
+    def test_df_rolling_kurt(self):
+        all_data = test_global_input_data_float64
+        length = min(len(d) for d in all_data)
+        data = {n: d[:length] for n, d in zip(string.ascii_uppercase, all_data)}
+        df = pd.DataFrame(data)
+
+        self._test_rolling_kurt(df)
 
     @skip_sdc_jit('DataFrame.rolling.max() unsupported')
     def test_df_rolling_max(self):
@@ -899,22 +923,11 @@ class TestRolling(TestCase):
 
     @skip_sdc_jit('Series.rolling.kurt() unsupported Series index')
     def test_series_rolling_kurt(self):
-        def test_impl(series, window, min_periods):
-            return series.rolling(window, min_periods).kurt()
-
-        hpat_func = self.jit(test_impl)
-
         all_data = test_global_input_data_float64
         indices = [list(range(len(data)))[::-1] for data in all_data]
         for data, index in zip(all_data, indices):
             series = pd.Series(data, index, name='A')
-            for window in range(4, len(series) + 1):
-                for min_periods in range(window + 1):
-                    with self.subTest(series=series, window=window,
-                                      min_periods=min_periods):
-                        ref_result = test_impl(series, window, min_periods)
-                        jit_result = hpat_func(series, window, min_periods)
-                        pd.testing.assert_series_equal(jit_result, ref_result)
+            self._test_rolling_kurt(series)
 
     @skip_sdc_jit('Series.rolling.max() unsupported Series index')
     def test_series_rolling_max(self):
