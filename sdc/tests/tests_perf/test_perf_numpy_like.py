5f3a8a50af02d5086e85a928b3d8133659f6a473
elena.totmenina <etotmeni@nntsatel03.inn.intel.com>
2020-01-30 11:38:06 +0300

    pep fix

--- sdc/tests/tests_perf/test_perf_numpy_like.py
+++ sdc/tests/tests_perf/test_perf_numpy_like.py
@@ -9,21 +9,23 @@ from sdc.functions.numpy_like import astype
 arr_float = np.arange(10 ** 8, dtype=float)
 arr_int = np.arange(10 ** 8, dtype=int)
 
-@jit(parallel=True) 
-def sdc(a, b): 
+
+@jit(parallel=True)
+def sdc(a, b):
     start_time = time.time()
     res = astype(a, np.int64)
     finish_time = time.time()
     print(res)
-    return finish_time - start_time        
+    return finish_time - start_time
 
-@jit(parallel=True) 
-def ref(a, b): 
+
+@jit(parallel=True)
+def ref(a, b):
     start_time = time.time()
     res = a.astype(np.int64)
     finish_time = time.time()
     print(res)
-    return finish_time - start_time  
+    return finish_time - start_time
 
 
 print(sdc(arr_float, arr_int))
