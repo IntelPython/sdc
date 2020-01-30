ffacf05a834be1da83c10d899424950d641f883f
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2020-01-30 10:07:50 +0300

    generic for perf_tests (#522)
    * add generator for all types
    
    * add Series.sub
    
    * add Series.mul
    
    * correction
    
    * correction
    
    * correction remarks on review
    
    * correction name
    
    * correction
    
    * correction parameter skipna fo min
    
    * correction generator
    
    * correction problem with PEP8
    
    * rename & add class for gen TestCase
    
    * corrction name case & correction class TestCase
    
    * rename input par
    
    * correct name
    
    * handle underscores
    
    * match to lines
    
    * combination of two functions
    
    * fix docstring for to_varname
    
    * fix gen func_name
    
    * fix docstring for var_name
    
    * add new func with replace excess underscores
    
    * rename some params
    
    * change
    
    * change input data
    
    * fix docstring
    
    * correct prefix
    
    * fix remarks
    
    * correct gen call_exp
    
    * change
    
    * correct name input parameter
    
    * correct data size for series
    
    * correct size data
    
    * change gen & change parameters in TC
    
    * change TC
    
    * change
    
    * change generator
    
    * add indent
    
    * add parameter skip of TestCase
    
    * correct dockstring
    
    * change
    
    * add TODO in TearnDown
    
    * change comment in test_perf_base
    
    * change in gen_test & gen_usecase
    
    Co-authored-by: Sergey Pokhodenko <pokhodenko.s.a@gmail.com>

---
+++ sdc/tests/tests_perf/generator.py
@@ -0,0 +1,111 @@
+import time
+import numpy as np
+from typing import NamedTuple
+from sdc.io.csv_ext import to_varname
+from sdc.tests.test_utils import *
+
+
+class TestCase(NamedTuple):
+    """
+    name: name of the API item, e.g. method, operator
+    size: size of the generated data for tests
+    params: method parameters in format 'par1, par2, ...'
+    call_expr: call expression as a string, e.g. '(A+B).sum()' where A, B are Series or DF
+    usecase_params: input parameters for usecase in format 'par1, par2, ...', e.g. 'data, other'
+    data_num: total number of generated data, e.g. 2 (data, other)
+    input_data: input data for generating test data
+    skip: flag for skipping a test
+    """
+    name: str
+    size: list
+    params: str = ''
+    call_expr: str = None
+    usecase_params: str = None
+    data_num: int = 1
+    input_data: list = None
+    skip: bool = False
+
+
+def to_varname_without_excess_underscores(string):
+    """Removing excess underscores from the string."""
+    return '_'.join(i for i in to_varname(string).split('_') if i)
+
+
+def generate_test_cases(cases, class_add, typ, prefix=''):
+    for test_case in cases:
+        test_name_parts = ['test', typ, prefix, test_case.name, gen_params_wo_data(test_case)]
+        test_name = to_varname_without_excess_underscores('_'.join(test_name_parts))
+
+        setattr(class_add, test_name, gen_test(test_case, prefix))
+
+
+def gen_params_wo_data(test_case):
+    """Generate API item parameters without parameters with data, e.g. without parameter other"""
+    extra_data_num = test_case.data_num - 1
+    method_params = test_case.params.split(', ')[extra_data_num:]
+
+    return ', '.join(method_params)
+
+
+def gen_usecase_params(test_case):
+    """Generate usecase parameters based on method parameters and number of extra generated data"""
+    extra_data_num = test_case.data_num - 1
+    extra_usecase_params = test_case.params.split(', ')[:extra_data_num]
+    usecase_params_parts = ['data'] + extra_usecase_params
+
+    return ', '.join(usecase_params_parts)
+
+
+def gen_call_expr(test_case, prefix):
+    """Generate call expression based on method name and parameters and method prefix, e.g. str"""
+    prefix_as_list = [prefix] if prefix else []
+    call_expr_parts = ['data'] + prefix_as_list + ['{}({})'.format(test_case.name, test_case.params)]
+
+    return '.'.join(call_expr_parts)
+
+
+def gen_test(test_case, prefix):
+    func_name = 'func'
+
+    usecase = gen_usecase(test_case, prefix)
+
+    skip = '@skip_numba_jit\n' if test_case.skip else ''
+
+    func_text = f"""
+{skip}def {func_name}(self):
+  self._test_case(usecase, name='{test_case.name}', total_data_length={test_case.size},
+                  data_num={test_case.data_num}, input_data={test_case.input_data})
+"""
+
+    loc_vars = {}
+    global_vars = {'usecase': usecase,
+                   'skip_numba_jit': skip_numba_jit}
+    exec(func_text, global_vars, loc_vars)
+    func = loc_vars[func_name]
+
+    return func
+
+
+def gen_usecase(test_case, prefix):
+    func_name = 'func'
+
+    usecase_params = test_case.usecase_params
+    call_expr = test_case.call_expr
+    if call_expr is None:
+        if usecase_params is None:
+            usecase_params = gen_usecase_params(test_case)
+        call_expr = gen_call_expr(test_case, prefix)
+
+    func_text = f"""
+def {func_name}({usecase_params}):
+  start_time = time.time()
+  res = {call_expr}
+  finish_time = time.time()
+  return finish_time - start_time, res
+"""
+
+    loc_vars = {}
+    exec(func_text, globals(), loc_vars)
+    func = loc_vars[func_name]
+
+    return func
