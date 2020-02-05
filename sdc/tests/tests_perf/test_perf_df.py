b097259a289962a08a87597e1491d40532644310
mrubtsov <maria.rubtsova@intel.com>
2020-02-05 11:52:40 +0300

    add_perf_test

--- sdc/tests/tests_perf/test_perf_df.py
+++ sdc/tests/tests_perf/test_perf_df.py
@@ -97,6 +97,7 @@ cases = [
     TC(name='append', size=[10 ** 7], params='other', data_num=2),
     TC(name='count', size=[10 ** 7]),
     TC(name='drop', size=[10 ** 8], params='columns="f0"'),
+    TC(name='isin', params='other', size=[10 ** 7], data_num=2),
     TC(name='max', size=[10 ** 7]),
     TC(name='mean', size=[10 ** 7]),
     TC(name='median', size=[10 ** 7]),
