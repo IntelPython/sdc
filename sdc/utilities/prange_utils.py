25182ca9fde5a7f531b517a5b0c32bdeaad0ef11
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-02-18 15:26:40 +0300

    Add tests for get_chunks and change formula for get_chunks

--- sdc/utilities/prange_utils.py
+++ sdc/utilities/prange_utils.py
@@ -29,7 +29,7 @@ import numba
 import sdc
 
 from typing import NamedTuple
-from sdc.utilities.utils import sdc_overload
+from sdc.utilities.utils import sdc_overload, sdc_register_jitable
 
 
 class Chunk(NamedTuple):
@@ -53,37 +53,19 @@ def get_pool_size_overload():
 
     return get_pool_size_impl
 
-
+@sdc_register_jitable
 def get_chunks(size, pool_size=0):
     if pool_size == 0:
         pool_size = get_pool_size()
 
-    chunk_size = (size - 1) // pool_size + 1
+    pool_size = min(pool_size, size)
+    chunk_size = size // pool_size
+    overload_size = size % pool_size
 
     chunks = []
     for i in range(pool_size):
-        start = min(i * chunk_size, size)
-        stop = min((i + 1) * chunk_size, size)
+        start = i * chunk_size + min(i, overload_size)
+        stop = (i + 1) * chunk_size + min(i + 1, overload_size)
         chunks.append(Chunk(start, stop))
 
     return chunks
-
-
-@sdc_overload(get_chunks)
-def get_chunks_overload(size, pool_size=0):
-    def get_chunks_impl(size, pool_size=0):
-        if pool_size == 0:
-            pool_size = get_pool_size()
-
-        chunk_size = (size - 1) // pool_size + 1
-
-        chunks = []
-        for i in range(pool_size):
-            start = min(i * chunk_size, size)
-            stop = min((i + 1) * chunk_size, size)
-            chunk = Chunk(start, stop)
-            chunks.append(chunk)
-
-        return chunks
-
-    return get_chunks_impl
