4c0abd2f61a558697cca8c898dbf58feb2adb870
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-02-19 13:16:06 +0300

    Add check_skipna

--- sdc/tests/tests_perf/generator.py
+++ sdc/tests/tests_perf/generator.py
@@ -26,6 +26,7 @@ class TestCase(NamedTuple):
     data_num: total number of generated data, e.g. 2 (data, other)
     input_data: input data for generating test data
     skip: flag for skipping a test
+    check_skipna: flag for checking a function with both parameters skipna=True and skipna=False
     """
     name: str
     size: list
@@ -35,6 +36,7 @@ class TestCase(NamedTuple):
     data_num: int = 1
     input_data: list = None
     skip: bool = False
+    check_skipna: bool = False
 
 
 def to_varname_without_excess_underscores(string):
@@ -42,8 +44,24 @@ def to_varname_without_excess_underscores(string):
     return '_'.join(i for i in to_varname(string).split('_') if i)
 
 
+def skipna_cases(cases):
+    """Generator. Replaces a test case containing check_skipna=True
+    with two cases containing parameters skipna=True and skipna=False
+    """
+    for case in cases:
+        if case.check_skipna:
+            for skipna in [True, False]:
+                params = case.params
+                if params:
+                    params += ', '
+                params += f'skipna={skipna}'
+                yield case._replace(params=params)
+        else:
+            yield case
+
+
 def generate_test_cases(cases, class_add, typ, prefix=''):
-    for test_case in cases:
+    for test_case in skipna_cases(cases):
         test_name_parts = ['test', typ, prefix, test_case.name, gen_params_wo_data(test_case)]
         test_name = to_varname_without_excess_underscores('_'.join(test_name_parts))
 
