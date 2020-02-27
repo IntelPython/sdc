5235fd90cba73de30a163425ffabf11c106a2f81
densmirn <53897164+densmirn@users.noreply.github.com>
2020-02-27 07:56:02 +0300

    Optimize series.rolling.min() (#613)
    * Optimize series.rolling.min()
    
    * Enable scalability for series.rolling.min()
    
    * Enable scalability for series.rolling.min()
    
    * Fix issue with indices

--- sdc/tests/tests_perf/test_perf_series_rolling.py
+++ sdc/tests/tests_perf/test_perf_series_rolling.py
@@ -86,6 +86,7 @@ class TestSeriesRollingMethods(TestBase):
         super().setUpClass()
         cls.map_ncalls_dlength = {
             'mean': (100, [8 * 10 ** 5]),
+            'min': (100, [4 * 10 ** 5]),
             'sum': (100, [8 * 10 ** 5]),
         }
 
@@ -128,6 +129,9 @@ class TestSeriesRollingMethods(TestBase):
     def test_series_rolling_mean(self):
         self._test_series_rolling_method('mean')
 
+    def test_series_rolling_min(self):
+        self._test_series_rolling_method('min')
+
     def test_series_rolling_sum(self):
         self._test_series_rolling_method('sum')
 
@@ -140,7 +144,6 @@ cases = [
     TC(name='kurt', size=[10 ** 7]),
     TC(name='max', size=[10 ** 7]),
     TC(name='median', size=[10 ** 7]),
-    TC(name='min', size=[10 ** 7]),
     TC(name='quantile', size=[10 ** 7], params='0.2'),
     TC(name='skew', size=[10 ** 7]),
     TC(name='std', size=[10 ** 7]),
