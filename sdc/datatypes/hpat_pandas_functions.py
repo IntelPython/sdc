f4c07226ea15638a18547194d7cb62f3743fec67
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-03-02 12:27:49 +0300

    Add implementation for sep

--- sdc/datatypes/hpat_pandas_functions.py
+++ sdc/datatypes/hpat_pandas_functions.py
@@ -57,13 +57,15 @@ def infer_usecols(col_names):
 
 
 @overload(pandas.read_csv)
-def sdc_pandas_read_csv(fname, skiprows=0):
-    signature =        "fname, skiprows=0"  # nopep8
+def sdc_pandas_read_csv(fname, sep=',', skiprows=0):
+    signature =        "fname, sep=',', skiprows=0"  # nopep8
 
     assert isinstance(fname, numba.types.Literal)
+    assert isinstance(sep, numba.types.Literal) or sep == ','
     assert isinstance(skiprows, numba.types.Literal) or skiprows == 0
 
-    sep = ','
+    if isinstance(sep, numba.types.Literal):
+        sep = sep.literal_value
 
     fname_const = fname.literal_value
     if isinstance(skiprows, numba.types.Literal):
@@ -71,7 +73,7 @@ def sdc_pandas_read_csv(fname, skiprows=0):
     col_names = 0
     skiprows, col_names, dtype_map = \
         HiFramesPassImpl.infer_column_names_and_types_from_constant_filename(
-            fname_const, skiprows, col_names)
+            fname_const, skiprows, col_names, sep=sep)
 
     usecols = infer_usecols(col_names)
 
