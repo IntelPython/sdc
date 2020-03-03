a1d3287bf7c0c6009905364fff77d70606ce6aad
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2020-03-03 19:03:30 +0300

    Optimize series rolling count (#626)
    Optimize Series.Rolling.count

--- sdc/tests/tests_perf/test_perf_series_rolling.py
+++ sdc/tests/tests_perf/test_perf_series_rolling.py
@@ -85,6 +85,7 @@ class TestSeriesRollingMethods(TestBase):
     def setUpClass(cls):
         super().setUpClass()
         cls.map_ncalls_dlength = {
+            'count': (100, [8 * 10 ** 5]),
             'kurt': (100, [8 * 10 ** 5]),
             'mean': (100, [8 * 10 ** 5]),
             'min': (100, [4 * 10 ** 5]),
@@ -130,6 +131,9 @@ class TestSeriesRollingMethods(TestBase):
             data_num += len(extra_usecase_params.split(', '))
         self._test_case(usecase, name, total_data_length, data_num=data_num)
 
+    def test_series_rolling_count(self):
+        self._test_series_rolling_method('count')
+
     def test_series_rolling_kurt(self):
         self._test_series_rolling_method('kurt')
 
@@ -155,7 +159,6 @@ class TestSeriesRollingMethods(TestBase):
 cases = [
     TC(name='apply', size=[10 ** 7], params='func=lambda x: np.nan if len(x) == 0 else x.mean()'),
     TC(name='corr', size=[10 ** 7]),
-    TC(name='count', size=[10 ** 7]),
     TC(name='cov', size=[10 ** 7]),
     TC(name='max', size=[10 ** 7]),
     TC(name='median', size=[10 ** 7]),
