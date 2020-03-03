469deefbbd584e9d7c8a3fde8092e11b6dc27c27
akharche <angelina.kharchevnikova@intel.com>
2020-03-03 13:19:57 +0300

    Fix pep

--- sdc/datatypes/hpat_pandas_dataframe_functions.py
+++ sdc/datatypes/hpat_pandas_dataframe_functions.py
@@ -921,7 +921,8 @@ def sdc_pandas_dataframe_drop_codegen(func_name, func_args, df, drop_cols):
             break
 
     for column_id, column_name in enumerate(saved_df_columns):
-        func_text.append(f'new_col_{column_id}_data_{"df"} = get_dataframe_data({"df"}, {df_columns_indx[column_name]})')
+        func_text.append(f'new_col_{column_id}_data_{"df"} = get_dataframe_data({"df"}, '
+                         f'{df_columns_indx[column_name]})')
         column_list.append((f'new_col_{column_id}_data_df', column_name))
 
     data = ', '.join(f'"{column_name}": {column}' for column, column_name in column_list)
