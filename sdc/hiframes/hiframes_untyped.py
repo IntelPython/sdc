c3a9a24c06f8ca397522e2abb7916a2085417b81
Alexander Kalistratov <alexander.kalistratov@intel.com>
2020-03-05 11:30:04 +0300

    Read csv delimiter (#660)
    * Print folder where data is generated
    
    * Remove code duplication in test_io TestCSV
    
    * Use dtype.name for _int_type_str
    
    * Add test for sep
    
    * Add implementation for sep
    
    * Generate csv_data_infer_sep.csv
    
    * Call generate_csv_data() from main
    
    * Add support of delimiter parameter

--- sdc/hiframes/hiframes_untyped.py
+++ sdc/hiframes/hiframes_untyped.py
@@ -757,9 +757,9 @@ class HiFramesPassImpl(object):
         return dtypes
 
     @staticmethod
-    def infer_column_names_and_types_from_constant_filename(fname_const, skiprows, col_names):
+    def infer_column_names_and_types_from_constant_filename(fname_const, skiprows, col_names, sep=','):
         rows_to_read = 100  # TODO: tune this
-        df = pd.read_csv(fname_const, nrows=rows_to_read, skiprows=skiprows)
+        df = pd.read_csv(fname_const, sep=sep, skiprows=skiprows, nrows=rows_to_read)
         # TODO: string_array, categorical, etc.
         dtypes = HiFramesPassImpl.get_dtypes(df)
         cols = df.columns.to_list()
