0ad374ec0ab53de1f9335d7fd20f83369ae92ae2
elena.totmenina <etotmeni@nntsatel03.inn.intel.com>
2020-03-12 16:29:23 +0300

    test fix

--- sdc/tests/test_dataframe.py
+++ sdc/tests/test_dataframe.py
@@ -994,15 +994,15 @@ class TestDataFrame(TestCase):
         self.assertEqual(sdc_func(df), test_impl(df))
 
     def test_df_iat_value_error(self):
-        def test_impl(df, n):
-            return df.iat[n, 1]
+        def test_impl(df):
+            return df.iat[1, 22]
         sdc_func = sdc.jit(test_impl)
         df = pd.DataFrame({"A": [3.2, 4.4, 7.0, 3.3, 1.0],
                            "B": [3, 4, 1, 0, 222],
                            "C": ['a', 'dd', 'c', '12', 'ddf']})
 
         with self.assertRaises(ValueError) as raises:
-            sdc_func(df, 22)
+            sdc_func(df)
         msg = 'Index is out of bounds for axis'
         self.assertIn(msg, str(raises.exception))
 
