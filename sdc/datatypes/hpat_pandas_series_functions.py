24158c663e3d67d04cfe2e50b401474e9044705a
Rubtsowa <mariyarubtsowa@yandex.ru>
2020-03-16 13:15:39 +0300

    delete note

--- sdc/datatypes/hpat_pandas_series_functions.py
+++ sdc/datatypes/hpat_pandas_series_functions.py
@@ -3893,10 +3893,6 @@ def hpat_pandas_series_idxmin(self, axis=None, skipna=None):
     .. command-output:: python ./series/series_idxmin.py
        :cwd: ../../../examples
 
-    .. note::
-
-        Parameter axis is currently unsupported by Intel Scalable Dataframe Compiler
-
     .. seealso::
 
         :ref:`Series.idxmax <pandas.Series.idxmax>`
