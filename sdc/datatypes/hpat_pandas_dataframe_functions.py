6da93f82ade7cd35ae67aed0406aa5de88fad1ff
Rubtsowa <mariyarubtsowa@yandex.ru>
2020-03-18 17:40:32 +0300

    change limitations

--- sdc/datatypes/hpat_pandas_dataframe_functions.py
+++ sdc/datatypes/hpat_pandas_dataframe_functions.py
@@ -1039,7 +1039,7 @@ def count_overload(df, axis=0, level=None, numeric_only=False):
 
     Limitations
     -----------
-    Parameters axis, level, numeric_only unsupported.
+    Parameters axis, level, numeric_only are unsupported.
 
     Examples
     --------
