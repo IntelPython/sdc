ea4770ee478f978d68d90d43208387aad98a0f63
Maidanov <sergey.maidanov@intel.com>
2020-03-18 11:50:17 -0500

    changes to user manual getting started

--- examples/basic_workflow_parallel.py
+++ examples/basic_workflow_parallel.py
@@ -25,7 +25,7 @@
 # *****************************************************************************
 
 import pandas as pd
-from numba import njit
+from numba import njit, prange
 
 # Dataset for analysis
 FNAME = "employees.csv"
@@ -37,7 +37,13 @@ def get_analyzed_data():
     df = pd.read_csv(FNAME)
     s_bonus = pd.Series(df['Bonus %'])
     s_first_name = pd.Series(df['First Name'])
-    m = s_bonus.mean()
+
+    # Use explicit loop to compute the mean. It will be compiled as parallel loop
+    m = 0.0
+    for i in prange(s_bonus.size):
+        m += s_bonus.values[i]
+    m /= s_bonus.size
+
     names = s_first_name.sort_values()
     return m, names
 
