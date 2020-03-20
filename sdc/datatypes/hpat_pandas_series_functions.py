0a93af5802cf09821469c4ef1f4fe31c288f2040
Rubtsowa <mariyarubtsowa@yandex.ru>
2020-03-20 12:43:31 +0300

    change doc

--- sdc/datatypes/hpat_pandas_series_functions.py
+++ sdc/datatypes/hpat_pandas_series_functions.py
@@ -279,46 +279,46 @@ def hpat_pandas_series_getitem(self, idx):
     Examples
     --------
     .. literalinclude:: ../../../examples/series/series_getitem/series_getitem_scalar_single_result.py
-        :language: python
-        :lines: 32-
-        :caption: Getting Pandas Series elements. Returns single value.
-        :name: ex_series_getitem
+       :language: python
+       :lines: 32-
+       :caption: Getting Pandas Series elements. Returns single value.
+       :name: ex_series_getitem
 
     .. command-output:: python ./series/series_getitem/series_getitem_scalar_single_result.py
        :cwd: ../../../examples
 
     .. literalinclude:: ../../../examples/series/series_getitem/series_getitem_scalar_multiple_result.py
-        :language: python
-        :lines: 34-
-        :caption: Getting Pandas Series elements. Returns multiple value.
-        :name: ex_series_getitem
+       :language: python
+       :lines: 34-
+       :caption: Getting Pandas Series elements. Returns multiple value.
+       :name: ex_series_getitem
 
     .. command-output:: python ./series/series_getitem/series_getitem_scalar_multiple_result.py
        :cwd: ../../../examples
 
     .. literalinclude:: ../../../examples/series/series_getitem/series_getitem_slice.py
-        :language: python
-        :lines: 35-
-        :caption: Getting Pandas Series elements by slice.
-        :name: ex_series_getitem
+       :language: python
+       :lines: 35-
+       :caption: Getting Pandas Series elements by slice.
+       :name: ex_series_getitem
 
     .. command-output:: python ./series/series_getitem/series_getitem_slice.py
        :cwd: ../../../examples
 
     .. literalinclude:: ../../../examples/series/series_getitem/series_getitem_bool_array.py
-        :language: python
-        :lines: 37-
-        :caption: Getting Pandas Series elements by array of booleans.
-        :name: ex_series_getitem
+       :language: python
+       :lines: 37-
+       :caption: Getting Pandas Series elements by array of booleans.
+       :name: ex_series_getitem
 
     .. command-output:: python ./series/series_getitem/series_getitem_bool_array.py
        :cwd: ../../../examples
 
     .. literalinclude:: ../../../examples/series/series_getitem/series_getitem_series.py
-        :language: python
-        :lines: 36-
-        :caption: Getting Pandas Series elements by another Series.
-        :name: ex_series_getitem
+       :language: python
+       :lines: 36-
+       :caption: Getting Pandas Series elements by another Series.
+       :name: ex_series_getitem
 
     .. command-output:: python ./series/series_getitem/series_getitem_series.py
        :cwd: ../../../examples
