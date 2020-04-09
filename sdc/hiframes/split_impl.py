4827e9896574a1db1cb1370133eeae7eceaeb29f
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2020-04-09 23:43:36 +0300

    delete some files in pd_series_ext.py (#795)

--- sdc/hiframes/split_impl.py
+++ sdc/hiframes/split_impl.py
@@ -483,24 +483,6 @@ def hpat_pandas_spliview_stringmethods_len(self):
     return hpat_pandas_spliview_stringmethods_len_impl
 
 
-# @infer_global(operator.getitem)
-class GetItemStringArraySplitView(AbstractTemplate):
-    key = operator.getitem
-
-    def generic(self, args, kws):
-        assert not kws
-        [ary, idx] = args
-        if ary == string_array_split_view_type:
-            if isinstance(idx, types.SliceType):
-                return signature(string_array_split_view_type, *args)
-            elif isinstance(idx, types.Integer):
-                return signature(types.List(string_type), *args)
-            elif idx == types.Array(types.bool_, 1, 'C'):
-                return signature(string_array_split_view_type, *args)
-            elif idx == types.Array(types.intp, 1, 'C'):
-                return signature(string_array_split_view_type, *args)
-
-
 @overload(operator.getitem)
 def str_arr_split_view_getitem_overload(A, ind):
     if A == string_array_split_view_type and isinstance(ind, types.Integer):
