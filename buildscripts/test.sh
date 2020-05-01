35ca6f63452fe988d9e629cb4624382b533cc44c
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-05-01 10:42:35 +0000

    Fix output path

--- buildscripts/test.sh
+++ buildscripts/test.sh
@@ -8,7 +8,8 @@ set -ex
 
 conda env update -f "$buildscripts/environment-build.yml"
 
-output=$(conda build --output-folder "$output_folder" --output "$recipe" | head -n 1)
+# output=$(conda build --output-folder "$output_folder" --output "$recipe" | head -n 1)
+output=$(ls $output_folder/linux-64/sdc*.tar.bz2)
 
 # conda build --test -c file:///workspaces/sdc/sdc-build -c intel/label/beta -c intel -c defaults /workspaces/sdc/sdc-build/linux-64/sdc-0.32.0-py36h0922cd1_28.tar.bz2
 conda build \
