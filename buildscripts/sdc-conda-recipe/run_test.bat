3c44e9f1dc7c2c55551bb9b029fee14704dd8fcc
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-05-12 16:37:36 +0000

    Run tests in separate processes

--- buildscripts/sdc-conda-recipe/run_test.bat
+++ buildscripts/sdc-conda-recipe/run_test.bat
@@ -9,10 +9,41 @@ if errorlevel 1 exit 1
 
 @rem TODO investigate root cause of NumbaPerformanceWarning
 @rem http://numba.pydata.org/numba-doc/latest/user/parallel.html#diagnostics
-python -W ignore -u -m sdc.runtests -v
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_basic
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_d4p
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_dataframe
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_date
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_groupby
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_hiframes
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_hpat_jit
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_io
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_join
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_ml
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_prange_utils
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_rolling
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_sdc_numpy
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_series_apply
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_series_map
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_series
+if errorlevel 1 exit 1
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_strings
 if errorlevel 1 exit 1
 
 REM Link check for Documentation using Sphinx's in-built linkchecker
 REM sphinx-build -b linkcheck -j1 usersource _build/html
 REM if errorlevel 1 exit 1
-
--- buildscripts/sdc-conda-recipe/run_test.sh
+++ buildscripts/sdc-conda-recipe/run_test.sh
@@ -13,4 +13,20 @@ python -m sdc.tests.gen_test_data
 
 # TODO investigate root cause of NumbaPerformanceWarning
 # http://numba.pydata.org/numba-doc/latest/user/parallel.html#diagnostics
-python -W ignore -u -m sdc.runtests -v
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_basic
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_d4p
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_dataframe
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_date
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_groupby
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_hiframes
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_hpat_jit
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_io
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_join
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_ml
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_prange_utils
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_rolling
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_sdc_numpy
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_series_apply
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_series_map
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_series
+python -W ignore -u -m sdc.runtests -v sdc.tests.test_strings
