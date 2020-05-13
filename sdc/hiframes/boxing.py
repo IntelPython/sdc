8afd53b8727b83289a5a1f1731b9d3301c0a942c
etotmeni <elena.totmenina@intel.com>
2020-05-13 17:19:38 +0300

    Add decrefs in boxing

--- sdc/hiframes/boxing.py
+++ sdc/hiframes/boxing.py
@@ -238,13 +238,14 @@ def box_dataframe(typ, val, c):
             # context.nrt.incref(builder, arr_typ, arr)
         pyapi.object_setitem(df_obj, cname_obj, arr_obj)
 
-        # pyapi.decref(arr_obj)
+        pyapi.decref(arr_obj)
         pyapi.decref(cname_obj)
 
     # set df.index if necessary
     if typ.index != types.none:
         arr_obj = _box_series_data(typ.index.dtype, typ.index, dataframe.index, c)
         pyapi.object_setattr_string(df_obj, 'index', arr_obj)
+        pyapi.decref(arr_obj)
 
     pyapi.decref(class_obj)
     # pyapi.gil_release(gil_state)    # release GIL
@@ -350,6 +351,10 @@ def box_series(typ, val, c):
     res = c.pyapi.call_method(
         pd_class_obj, "Series", (arr, index, dtype, name))
 
+    c.pyapi.decref(arr)
+    c.pyapi.decref(index)
+    c.pyapi.decref(dtype)
+    c.pyapi.decref(name)
     c.pyapi.decref(pd_class_obj)
     return res
 
