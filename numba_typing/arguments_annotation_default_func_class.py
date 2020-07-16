77901db1c7e76d341eda44cef068f1184fb640a5
KsanaKozlova <oksana.kozlova@intel.com>
2020-07-16 13:23:58 +0300

    add getting annotation for func and classes

---
+++ numba_typing/arguments_annotation_default_func_class.py
@@ -0,0 +1,26 @@
+from inspect import signature, getfullargspec
+from typing import get_type_hints
+
+
+def get_arguments_annotation_default_func(method):
+    sig = signature(method)
+    argspec = getfullargspec(method)
+    arg_index=0
+    annotations = {}
+    defaults = {}
+
+    for arg in argspec.args:
+        if (sig.parameters[arg].annotation == sig.empty):
+            raise SyntaxError
+        else:
+            annotations[arg] =  sig.parameters[arg].annotation
+    for arg in argspec.args:
+        if (sig.parameters[arg].default != sig.empty):
+            defaults[arg] =  sig.parameters[arg].default
+    
+    return annotations, defaults
+
+        
+def get_arguments_annotation_class(cls):
+    return get_type_hints(cls)
+
