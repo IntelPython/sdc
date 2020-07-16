c1f5f8a10d77fdbd1534951a7120d636052bede5
Perevezentsev Vladislav <vladislav.perevezentsev@intel.com>
2020-07-16 15:53:20 +0300

    pep8 fix

--- numba_typing/ast_example.py
+++ numba_typing/ast_example.py
@@ -10,23 +10,23 @@ def main():
     analyzer.visit(tree)
     print(analyzer.types)
 
+
 class Analyzer(ast.NodeVisitor):
     def __init__(self):
         self.types = {}
         self.flag = False
 
-
-    def visit_FunctionDef(self,obj):
+    def visit_FunctionDef(self, obj):
         self.flag = True
         for i in obj.body:
             self.visit(i)
 
-    def visit_AnnAssign(self,node):
+    def visit_AnnAssign(self, node):
         if self.flag == True:
-        #need to check for child functions
-            if isinstance(node.annotation,ast.Subscript):                       #containers and generics
+            # need to check for child functions
+            if isinstance(node.annotation, ast.Subscript):  # containers and generics
                 container_name = node.annotation.value.id
-                if isinstance(node.annotation.slice.value,ast.Tuple):   
+                if isinstance(node.annotation.slice.value, ast.Tuple):
                     type_list = []
                     for t in node.annotation.slice.value.elts:
                         type_list.append(t.id)
@@ -35,14 +35,11 @@ class Analyzer(ast.NodeVisitor):
                 else:
                     self.types[node.target.id] = [eval(f'{container_name}[{node.annotation.slice.value.id}]')]
             else:
-                try:                                                               #not containers
-                    self.types[node.target.id]=[eval(node.annotation.id)]  
+                try:  # not containers
+                    self.types[node.target.id] = [eval(node.annotation.id)]
                 except TypeError:
-                    self.types[node.target.id]=[eval(node.annotation.id)]                #if Any type
+                    self.types[node.target.id] = [eval(node.annotation.id)]  # if Any type
 
 
 if __name__ == '__main__':
     main()
-
-
-
--- numba_typing/test_generics.py
+++ numba_typing/test_generics.py
@@ -7,19 +7,17 @@ foo_bool: bool = True
 foo_bytes: bytes = b'\x00'
 foo_global: int = 30
 
-foo_list: List[str] = ['Hello','world']
+foo_list: List[str] = ['Hello', 'world']
 foo_tuple: Tuple[int] = (1,)
-foo_dict: Dict[str,int] = {'Hello':1}
+foo_dict: Dict[str, int] = {'Hello': 1}
+
 
 def qwe():
     qwe_int: int = 10
     qwe_int_op_n: Optional[int]
     qwe_any: Any
-    qwe_l: List[int] = [1,2,3]
-    qwe_list: Optional[int] = [1,2,3]
-    qwe_list_u: Union[int,float] 
-    qwe_tuple: Union[str,int] = ("qqq",1)
-    qwe_dict: Union[str,str] = {'hello':'world'}
-
-
-
+    qwe_l: List[int] = [1, 2, 3]
+    qwe_list: Optional[int] = [1, 2, 3]
+    qwe_list_u: Union[int, float]
+    qwe_tuple: Union[str, int] = ("qqq", 1)
+    qwe_dict: Union[str, str] = {'hello': 'world'}
