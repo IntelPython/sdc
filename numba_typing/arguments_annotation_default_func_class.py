53a2b307c96986ab4d307c08dfe1098d7b33dd29
KsanaKozlova <oksana.kozlova@intel.com>
2020-07-16 14:25:16 +0300

    fix pep8 remarks

--- numba_typing/arguments_annotation_default_func_class.py
+++ numba_typing/arguments_annotation_default_func_class.py
@@ -5,7 +5,7 @@ from typing import get_type_hints
 def get_arguments_annotation_default_func(method):
     sig = signature(method)
     argspec = getfullargspec(method)
-    arg_index=0
+    arg_index = 0
     annotations = {}
     defaults = {}
 
@@ -13,14 +13,12 @@ def get_arguments_annotation_default_func(method):
         if (sig.parameters[arg].annotation == sig.empty):
             raise SyntaxError
         else:
-            annotations[arg] =  sig.parameters[arg].annotation
+            annotations[arg] = sig.parameters[arg].annotation
     for arg in argspec.args:
         if (sig.parameters[arg].default != sig.empty):
-            defaults[arg] =  sig.parameters[arg].default
+            defaults[arg] = sig.parameters[arg].default
     
     return annotations, defaults
-
         
 def get_arguments_annotation_class(cls):
     return get_type_hints(cls)
-
