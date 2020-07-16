534c69904327c7db2b6a36f46f2d526ab9cb3880
Perevezentsev Vladislav <vladislav.perevezentsev@intel.com>
2020-07-16 14:33:24 +0300

    getting local variable annotations of function and generics

---
+++ numba_typing/ast_example.py
@@ -0,0 +1,48 @@
+import ast
+import inspect
+from typing import Optional, Any, Union, List, Tuple, Dict
+
+
+def main():
+    with open('test_generics.py') as f:
+        tree = ast.parse(f.read())
+    analyzer = Analyzer()
+    analyzer.visit(tree)
+    print(analyzer.types)
+
+class Analyzer(ast.NodeVisitor):
+    def __init__(self):
+        self.types = {}
+        self.flag = False
+
+
+    def visit_FunctionDef(self,obj):
+        self.flag = True
+        for i in obj.body:
+            self.visit(i)
+
+    def visit_AnnAssign(self,node):
+        if self.flag == True:
+        #need to check for child functions
+            if isinstance(node.annotation,ast.Subscript):                       #containers and generics
+                container_name = node.annotation.value.id
+                if isinstance(node.annotation.slice.value,ast.Tuple):   
+                    type_list = []
+                    for t in node.annotation.slice.value.elts:
+                        type_list.append(t.id)
+                    list_to_str = ','.join(type_list)
+                    self.types[node.target.id] = [eval(f'{container_name}[{list_to_str}]')]
+                else:
+                    self.types[node.target.id] = [eval(f'{container_name}[{node.annotation.slice.value.id}]')]
+            else:
+                try:                                                               #not containers
+                    self.types[node.target.id]=[eval(node.annotation.id)]  
+                except TypeError:
+                    self.types[node.target.id]=[eval(node.annotation.id)]                #if Any type
+
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ numba_typing/test_generics.py
@@ -0,0 +1,25 @@
+from typing import Optional, Any, Union, List, Tuple, Dict
+
+foo_int: int = 10
+foo_str: str = 'ten'
+foo_float: float = 10.5
+foo_bool: bool = True
+foo_bytes: bytes = b'\x00'
+foo_global: int = 30
+
+foo_list: List[str] = ['Hello','world']
+foo_tuple: Tuple[int] = (1,)
+foo_dict: Dict[str,int] = {'Hello':1}
+
+def qwe():
+    qwe_int: int = 10
+    qwe_int_op_n: Optional[int]
+    qwe_any: Any
+    qwe_l: List[int] = [1,2,3]
+    qwe_list: Optional[int] = [1,2,3]
+    qwe_list_u: Union[int,float] 
+    qwe_tuple: Union[str,int] = ("qqq",1)
+    qwe_dict: Union[str,str] = {'hello':'world'}
+
+
+
