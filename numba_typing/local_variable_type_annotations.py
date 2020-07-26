a88e460dedea2812610d9dd8f368b27c864299c5
Perevezentsev Vladislav <vladislav.perevezentsev@intel.com>
2020-07-26 23:14:39 +0300

    add unit tests for user types and correctly handles Typevar type

---
+++ numba_typing/local_variable_type_annotations.py
@@ -0,0 +1,55 @@
+import ast
+import inspect
+import textwrap
+from pathlib import Path
+
+
+def get_variable_annotations(func):
+    """Get local variable annotations from the function."""
+    module_path = inspect.getfile(func)
+    module_object = inspect.getmodule(func)
+    module_name = (Path(f'{module_path}').stem)
+    func_code = inspect.getsource(func)
+    func_code_with_dedent = textwrap.dedent(func_code)
+    func_tree = ast.parse(func_code_with_dedent)
+    analyzer = Analyzer(module_name, module_object)
+    analyzer.visit(func_tree)
+    return analyzer.locals_parameter
+
+
+class Analyzer(ast.NodeVisitor):
+    def __init__(self, module_name, module_object):
+        self.locals_parameter = {}
+        self.module_name = module_name
+        self.module_object = module_object
+        self.global_parameter = {self.module_name: self.module_object}
+
+    def visit_AnnAssign(self, node):
+        target, annotation = node.target, node.annotation
+        if isinstance(annotation, ast.Subscript):  # containers and generics
+            try:
+                container_name = annotation.value.id
+                if isinstance(annotation.slice.value, ast.Tuple):
+                    types_as_str = ','.join(elt.id for elt in annotation.slice.value.elts)
+                    exec_variables = f'{target.id} = {self.module_name}.{container_name}[{types_as_str}]'
+                else:
+                    exec_variables = f'{target.id} = {self.module_name}.{container_name}[{annotation.slice.value.id}]'
+            except AttributeError:  # typing.
+                module_import_name = annotation.value.value.id
+                container_name = annotation.value.attr
+                if isinstance(annotation.slice.value, ast.Tuple):
+                    types_as_str = ','.join(elt.id for elt in annotation.slice.value.elts)
+                    exec_variables = f'{target.id} = {module_import_name}.{container_name}[{types_as_str}]'
+                else:
+                    exec_variables = f'{target.id} = {self.module_name}.{module_import_name}.{container_name}[{annotation.slice.value.id}]'
+            exec(exec_variables, self.global_parameter, self.locals_parameter)
+        else:  # not containers
+            try:
+                exec(f'{target.id} = {annotation.id}', self.global_parameter, self.locals_parameter)
+            except NameError:  # if Any type
+                exec_variables = f'{target.id} = {self.module_name}.{annotation.id}'
+                exec(exec_variables, self.global_parameter, self.locals_parameter)
+
+
+if __name__ == '__main__':
+    ...
---
+++ numba_typing/test_variable_annotations.py
@@ -0,0 +1,84 @@
+from ast_example import get_variable_annotations
+import test_generics
+import unittest
+from typing import Any, Union, List, Tuple, Dict, Iterable, Iterator, Generic, TypeVar
+import typing
+
+T = TypeVar('T')
+S = TypeVar('S', int, str)
+G = Generic[T, S]
+
+
+class A():
+    ...
+
+
+class B():
+    ...
+
+
+class C():
+    ...
+
+
+class TestAst(unittest.TestCase):
+    maxDiff = None
+
+    def test_get_variable_annotations_with_test_generics(self):
+        result = get_variable_annotations(test_generics.qwe)
+        expected_result = {'qwe_int': int, 'qwe_list': test_generics.List[int],
+                           'qwe_tuple': test_generics.Tuple[str, int], 'qwe_dict': test_generics.Dict[str, str],
+                           'qwe_any': test_generics.Any, 'qwe_int_op': test_generics.typing.Optional[int],
+                           'qwe_int_float_un': test_generics.Union[int, float], 'T_func': test_generics.T,
+                           'S_func': test_generics.S, 'G_func': test_generics.G,
+                           'User_func': test_generics.UserId, 'class_func': test_generics.A}
+        self.assertEqual(result, expected_result)
+
+    def test_get_variable_annotations_standard_types(self):
+        def test_func():
+            t_int: int
+            t_str: str
+            t_float: float
+            t_bool: bool
+            t_bytes: bytes
+            t_list: list
+            t_dict: dict
+            t_tuple: tuple
+            t_set: set
+        result = get_variable_annotations(test_func)
+        expected_result = {'t_int': int, 't_str': str, 't_float': float, 't_bool': bool, 't_bytes': bytes,
+                           't_list': list, 't_dict': dict, 't_tuple': tuple, 't_set': set}
+        self.assertEqual(result, expected_result)
+
+    def test_get_variable_annotations_generic_types(self):
+        def test_func():
+            t_any: Any
+            t_union: Union[str, bytes]
+            t_optional: typing.Optional[float]
+            t_list: List[int]
+            t_tuple: Tuple[str]
+            t_dict: Dict[str, str]
+            t_iterable: Iterable[float]
+            t_iterator: Iterator[int]
+            t_generic: G
+            t_typevar_t: T
+            t_typevar_s: S
+        result = get_variable_annotations(test_func)
+        expected_result = {'t_any': Any, 't_union': Union[str, bytes], 't_optional': typing.Optional[float], 't_list': List[int],
+                           't_tuple': Tuple[str], 't_dict': Dict[str, str], 't_iterable': Iterable[float],
+                           't_iterator': Iterator[int], 't_generic': G, 't_typevar_t': T, 't_typevar_s': S}
+        self.assertEqual(result, expected_result)
+
+    def test_get_variable_annotations_user_types(self):
+
+        def test_func():
+            t_class_a: A
+            t_class_b: B
+            t_class_c: C
+        result = get_variable_annotations(test_func)
+        expected_result = {'t_class_a': A, 't_class_b': B, 't_class_c': C}
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
