ef7e313482cf340d615bd87e43731c856ec2eee1
KsanaKozlova <oksana.kozlova@intel.com>
2020-07-29 08:23:30 +0300

    add function expend_annotations

--- numba_typing/type_annotations.py
+++ numba_typing/type_annotations.py
@@ -1,5 +1,7 @@
 from inspect import signature
 from typing import get_type_hints, Union
+from itertools import product
+from copy import deepcopy
 
 
 def get_func_annotations(func):
@@ -10,7 +12,7 @@ def get_func_annotations(func):
 
     for name, param in sig.parameters.items():
         if param.annotation == sig.empty:
-            raise SyntaxError(f'Not found annotation for parameter {name}')
+            raise SyntaxError(f'No annotation for parameter {name}')
 
         annotations[name] = get_annotation_types(param.annotation)
         if param.default != sig.empty:
@@ -36,3 +38,52 @@ def get_annotation_types(annotation):
         pass
 
     return [annotation, ]
+
+
+def expend_annotations(info):
+    """Get all variants of annotations."""
+    annot = info[0]
+    vals = info[1]
+    list_of_annot = list(product(*annot.values()))
+    tvs = {}
+    tvs_unique = {}
+    count = 1
+    for x in annot:
+        for y in annot[x]:
+            if isinstance(y, TypeVar) and y.__constraints__ != ():
+                if x in tvs:
+                    tvs[x].append(y)
+                else:
+                    tvs[x] = [y, ]
+                if y not in tvs_unique.keys():
+                    tvs_unique[y] = y.__constraints__
+                    count *= len(y.__constraints__)
+
+    prod = list(product(*tvs_unique.values()))
+    temp_res = []
+
+    for i in range(len(list_of_annot)):
+        temp = []
+        temp_dict = {}
+        num = 0
+        for attr in annot:
+            temp_dict[attr] = list_of_annot[i][num]
+            num += 1
+        temp.append(temp_dict)
+        temp.append(vals)
+        temp_res.append(temp)
+
+    result = []
+    for examp in temp_res:
+        for i in range(count):
+            result.append(deepcopy(examp))
+
+    types = list(tvs_unique.keys())
+    for k in range(len(result)):
+        pos = k % count
+        for x in result[k][0]:
+            for i in range(len(prod[pos])):
+                if result[k][0][x] == types[i]:
+                    result[k][0][x] = prod[pos][i]
+
+    return result
