05c745ba75619286d6efa6f640a8d4ae1d93ec9d
Perevezentsev Vladislav <vladislav.perevezentsev@intel.com>
2020-09-09 01:19:50 +0300

    add default value processing and tests

--- numba_typing/overload_list.py
+++ numba_typing/overload_list.py
@@ -24,9 +24,13 @@ def overload_list(orig_func):
             for func in func_list:
                 sig_list.append((product_annotations(
                     get_func_annotations(func)), func))
-            param = getfullargspec(orig_func).args
-            kwargs = {name: typ for name, typ in zip(param, args)}
-            result = choose_func_by_sig(sig_list, **kwargs)
+            args_orig_func = getfullargspec(orig_func)
+            values_dict = {name: typ for name, typ in zip(args_orig_func.args, args)}
+            defaults_dict = {}
+            if args_orig_func.defaults:
+                defaults_dict = {name: value for name, value in zip(
+                    args_orig_func.args[::-1], args_orig_func.defaults[::-1])}
+            result = choose_func_by_sig(sig_list, values_dict, defaults_dict)
 
             if result is None:
                 raise numba.TypingError(f'Unsupported types a={a}, b={b}')
@@ -36,60 +40,6 @@ def overload_list(orig_func):
         return overload(orig_func, strict=False)(wrapper)
 
     return overload_inner
-
-
-class TypeChecker:
-    def __init__(self):
-        self._types_dict = {int: check_int_type, float: check_float_type, bool: check_bool_type,
-                            str: check_str_type, list: check_list_type,
-                            tuple: check_tuple_type, dict: check_dict_type}
-        self._typevars_dict = {}
-
-    def add_type_check(self, type_check, func):
-        self._types_dict[type_check] = func
-
-    def _is_generic(self, p_obj):
-        if isinstance(p_obj, typing._GenericAlias):
-            return True
-
-        if isinstance(p_obj, typing._SpecialForm):
-            return p_obj not in {typing.Any}
-
-        return False
-
-    def _get_origin(self, p_obj):
-        return p_obj.__origin__
-
-    def match(self, p_type, n_type):
-        try:
-            if p_type == typing.Any:
-                return True
-            elif self._is_generic(p_type):
-                origin_type = self._get_origin(p_type)
-                if origin_type == typing.Generic:
-                    return self.match_generic(p_type, n_type)
-                else:
-                    return self._types_dict[origin_type](self, p_type, n_type)
-            elif isinstance(p_type, typing.TypeVar):
-                return self.match_typevar(p_type, n_type)
-            else:
-                if p_type in (list, tuple):
-                    return self._types_dict[p_type](self, p_type, n_type)
-                return self._types_dict[p_type](n_type)
-        except KeyError:
-            print((f'A check for the {p_type} was not found'))
-
-    def match_typevar(self, p_type, n_type):
-        if not self._typevars_dict.get(p_type) and n_type not in self._typevars_dict.values():
-            self._typevars_dict[p_type] = n_type
-            return True
-        return self._typevars_dict.get(p_type) == n_type
-
-    def match_generic(self, p_type, n_type):
-        res = True
-        for arg in p_type.__args__:
-            res = res and self.match(arg, n_type)
-        return res
 
 
 def check_int_type(n_type):
@@ -144,12 +94,71 @@ def check_dict_type(self, p_type, n_type):
     return res
 
 
-def choose_func_by_sig(sig_list, **kwargs):
+class TypeChecker:
+
+    _types_dict = {int: check_int_type, float: check_float_type, bool: check_bool_type,
+                   str: check_str_type, list: check_list_type,
+                   tuple: check_tuple_type, dict: check_dict_type}
+
+    def __init__(self):
+        self._typevars_dict = {}
+
+    def clear_typevars_dict(self):
+        self._typevars_dict.clear()
+
+    def add_type_check(self, type_check, func):
+        self._types_dict[type_check] = func
+
+    def _is_generic(self, p_obj):
+        if isinstance(p_obj, typing._GenericAlias):
+            return True
+
+        if isinstance(p_obj, typing._SpecialForm):
+            return p_obj not in {typing.Any}
+
+        return False
+
+    def _get_origin(self, p_obj):
+        return p_obj.__origin__
+
+    def match(self, p_type, n_type):
+        try:
+            if p_type == typing.Any:
+                return True
+            elif self._is_generic(p_type):
+                origin_type = self._get_origin(p_type)
+                if origin_type == typing.Generic:
+                    return self.match_generic(p_type, n_type)
+                else:
+                    return self._types_dict[origin_type](self, p_type, n_type)
+            elif isinstance(p_type, typing.TypeVar):
+                return self.match_typevar(p_type, n_type)
+            else:
+                if p_type in (list, tuple):
+                    return self._types_dict[p_type](self, p_type, n_type)
+                return self._types_dict[p_type](n_type)
+        except KeyError:
+            print((f'A check for the {p_type} was not found. {n_type}'))
+
+    def match_typevar(self, p_type, n_type):
+        if not self._typevars_dict.get(p_type) and n_type not in self._typevars_dict.values():
+            self._typevars_dict[p_type] = n_type
+            return True
+        return self._typevars_dict.get(p_type) == n_type
+
+    def match_generic(self, p_type, n_type):
+        res = True
+        for arg in p_type.__args__:
+            res = res and self.match(arg, n_type)
+        return res
+
+
+def choose_func_by_sig(sig_list, values_dict, defaults_dict={}):
+    checker = TypeChecker()
     for sig in sig_list:  # sig = (Signature,func)
-        checker = TypeChecker()
         for param in sig[0].parameters:  # param = {'a':int,'b':int}
             full_match = True
-            for name, typ in kwargs.items():  # name,type = 'a',int64
+            for name, typ in values_dict.items():  # name,type = 'a',int64
                 if isinstance(typ, types.Literal):
 
                     full_match = full_match and checker.match(
@@ -162,6 +171,12 @@ def choose_func_by_sig(sig_list, **kwargs):
 
                 if not full_match:
                     break
+
+            for name, val in defaults_dict.items():
+                if sig[0].defaults.get(name) != None:
+                    full_match = full_match and sig[0].defaults[name] == val
+
+            checker.clear_typevars_dict()
             if full_match:
                 return sig[1]
 
--- numba_typing/test_overload_list.py
+++ numba_typing/test_overload_list.py
@@ -45,13 +45,15 @@ def foo_ovld_list():
     def foo_union(a: typing.Union[int, str], b: typing.Union[float, bool] = None):
         return('union', a, b)
 
-    def foo_optional(a: typing.Optional[float], b: typing.Optional[str] = None):
-        return('optional', a, b)
+    # def foo_optional(a: typing.Optional[float], b: typing.Optional[str] = None):
+    #     return('optional', a, b)
 
-    def foo_list_in_list(a: typing.List[typing.List[int]], b: typing.List[typing.List[typing.List[float]]] = [[[0.0, 0.0]]]):
+    def foo_list_in_list(a: typing.List[typing.List[int]],
+                         b: typing.List[typing.List[typing.List[float]]] = [[[0.0, 0.0]]]):
         return('list_in_list', a, b)
 
-    def foo_tuple_in_tuple(a: typing.Tuple[typing.Tuple[int]], b: typing.Tuple[typing.Tuple[typing.Tuple[float]]] = ((0.0, 0.0))):
+    def foo_tuple_in_tuple(a: typing.Tuple[typing.Tuple[int]],
+                           b: typing.Tuple[typing.Tuple[typing.Tuple[float]]] = ((0.0, 0.0))):
         return('tuple_in_tuple', a, b)
 
     def foo_typevars_T_T(a: T, b: T):
@@ -69,12 +71,128 @@ def foo_ovld_list():
     def foo_typevars_list_dict_list(a: typing.List[T], b: typing.Dict[K, typing.List[T]]):
         return('TypeVars_List_Dict_List', a, b)
 
-    return foo_int, foo_float, foo_bool, foo_str, foo_list, foo_tuple, foo_dict, foo_union, foo_optional, foo_list_in_list, foo_tuple_in_tuple, foo_typevars_T_T, foo_typevars_list_T, foo_typevars_list_dict, foo_typevars_list_dict_list, foo_typevars_T_K
+    return foo_int, foo_float, foo_bool, foo_str, foo_list, foo_tuple, foo_dict, foo_union,\
+        foo_list_in_list, foo_tuple_in_tuple, foo_typevars_T_T, foo_typevars_list_T,\
+        foo_typevars_list_dict, foo_typevars_list_dict_list, foo_typevars_T_K
 
 
 @overload_list.njit
 def jit_func(a, b):
     return func(a, b)
+
+
+class TestOverloadListDefault(unittest.TestCase):
+    maxDiff = None
+
+    def test_myfunc_int_type_default(self):
+        def foo(a, b=0):
+            ...
+
+        @overload_list.overload_list(foo)
+        def foo_ovld_list():
+
+            def foo_int(a: int, b: int = 0):
+                return ('int', a, b)
+
+            return (foo_int,)
+
+        @overload_list.njit
+        def jit_func(a):
+            return foo(a)
+
+        self.assertEqual(jit_func(1), ('int', 1, 0))
+
+    def test_myfunc_float_type_default(self):
+        def foo(a, b=0.0):
+            ...
+
+        @overload_list.overload_list(foo)
+        def foo_ovld_list():
+
+            def foo_float(a: float, b: float = 0.0):
+                return ('float', a, b)
+
+            return (foo_float,)
+
+        @overload_list.njit
+        def jit_func(a):
+            return foo(a)
+
+        self.assertEqual(jit_func(1.0), ('float', 1.0, 0.0))
+
+    def test_myfunc_bool_type_default(self):
+        def foo(a, b=False):
+            ...
+
+        @overload_list.overload_list(foo)
+        def foo_ovld_list():
+
+            def foo_bool(a: bool, b: bool = False):
+                return ('bool', a, b)
+
+            return (foo_bool,)
+
+        @overload_list.njit
+        def jit_func(a):
+            return foo(a)
+
+        self.assertEqual(jit_func(True), ('bool', True, False))
+
+    def test_myfunc_str_type_default(self):
+        def foo(a, b='0'):
+            ...
+
+        @overload_list.overload_list(foo)
+        def foo_ovld_list():
+
+            def foo_str(a: str, b: str = '0'):
+                return ('str', a, b)
+
+            return (foo_str,)
+
+        @overload_list.njit
+        def jit_func(a):
+            return foo(a)
+
+        self.assertEqual(jit_func('qwe'), ('str', 'qwe', '0'))
+
+    # def test_myfunc_list_type_default(self):
+    #     warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
+    #     warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
+    #     L = List([0, 0])
+    #     def foo(a,b=L):
+    #         ...
+    #     @overload_list.overload_list(foo)
+    #     def foo_ovld_list():
+
+    #         def foo_list(a: typing.List[int], b: typing.List[int] = [0,0]):
+    #             return ('list', a, b)
+
+    #         return (foo_list,)
+
+    #     @overload_list.njit
+    #     def jit_func(a):
+    #         return foo(a)
+
+    #     self.assertEqual(jit_func([1,2]), ('list',[1,2],L))
+
+    def test_myfunc_tuple_type_default(self):
+        def foo(a, b=(0, 0)):
+            ...
+
+        @overload_list.overload_list(foo)
+        def foo_ovld_list():
+
+            def foo_tuple(a: tuple, b: tuple = (0, 0)):
+                return ('tuple', a, b)
+
+            return (foo_tuple,)
+
+        @overload_list.njit
+        def jit_func(a):
+            return foo(a)
+
+        self.assertEqual(jit_func((1, 2)), ('tuple', (1, 2), (0, 0)))
 
 
 class TestOverloadList(unittest.TestCase):
@@ -129,8 +247,8 @@ class TestOverloadList(unittest.TestCase):
     def test_myfunc_union_typing_str_float(self):
         self.assertEqual(jit_func('qwe', 2.0), ('union', 'qwe', 2.0))
 
-    def test_myfunc_optional_typing(self):
-        self.assertEqual(jit_func(1.0, 'qwe'), ('optional', 1.0, 'qwe'))
+    # def test_myfunc_optional_typing(self):
+    #     self.assertEqual(jit_func(1.0, 'qwe'), ('optional', 1.0, 'qwe'))
 
     def test_myfunc_list_in_list_type(self):
         L_int = List([List([1, 2])])
