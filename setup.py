c29b6644f22d09c67addf08ffab6214525460a2a
Kozlov, Alexey <alexey.kozlov@intel.com>
2020-09-23 16:21:53 +0300

    Migrating to Numba 0.51.2

--- setup.py
+++ setup.py
@@ -375,7 +375,7 @@ setup(name=SDC_NAME_STR,
           'numpy>=1.16',
           'pandas>=1.0',
           'pyarrow==0.17.0',
-          'numba>=0.50.1,<0.51',
+          'numba>=0.51.2,<0.52',
           'tbb'
           ],
       cmdclass=sdc_build_commands,
